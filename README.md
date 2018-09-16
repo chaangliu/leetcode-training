5ee1f95ed08f9d6dd7b06e6cd6f44fca61695418
liuchang <lcconan@gmail.com>
2018-09-16 14:45:47 +0800

    correct quicksort

--- README.md
+++ README.md
@@ -2,10 +2,9 @@
 
 This repository records solutions I submitted to leetcode. 
 
-Currently(Sept 15, 2018) it has 194 solutions, some of which were inspired by leetcode discuss or other places.
+Currently(Sept 15, 2018) it has 194 solutions, some of which were inspired from leetcode discuss or other places.
 
 
-I've been using [a blog](https://www.jianshu.com/nb/9897105) to track my thoughts.  
+Also, if you are interested, I've been using [a blog](https://www.jianshu.com/nb/9897105) to track my thoughts.  
 
--
 
--- basic/QuickSort.java
+++ basic/QuickSort.java
@@ -1,38 +1,96 @@
 package basic;
 
+import java.util.Arrays;
+
 /**
  * Created by DrunkPiano on 2016/12/25.
  */
 
-public class QuickSort{
-    public static void quickSort(int [] array , int low , int high ){
-        //
-        if(low >= high) return ;
-        int i = low , j = high;
-        int index = array[i];
-        while(i<j){
-            while (i<j && index<array[j])
-                j--;
-            if(i<j)
-                array[i++] = array[j];
-            while (i<j && index>array[i])
-                i++;
-            if (i<j)
-                array[j--] = array[i];
+public class QuickSort {
+
+    private static void quickSort(int[] a, int low, int high) {
+        //递归的出口
+        if (low > high) {
+            return;
         }
-        array[i] = index;
-        quickSort(array, low,i-1);
-        quickSort(array, i+1,high);
+        int i = low;
+        int j = high;
+        int pivot = a[low];
+        //完成一趟排序
+        while (i < j) {
+            //[从右往左]找到第一个小于pivot的数
+            while (i < j && a[j] > pivot) {
+                j--;
+            }
+            //从左往右找到第一个大于pivot的数
+            while (i < j && a[i] <= pivot) {
+                i++;
+            }
+            //a[i]和a[j]交换
+            if (i < j) {
+                int temp = a[i];
+                a[i] = a[j];
+                a[j] = temp;
+            }
+        }
+        //a[i]和a[low]交换
+        int temp = a[i];
+        a[i] = a[low];
+        a[low] = temp;
+        quickSort(a, low, i - 1);
+        quickSort(a, i + 1, high);
     }
 
-    public static void main(String args[]){
-        int i = 0 ;
-        int a[] = {3,4,1,2,6,1,3,6};
-        int len = a.length;
-        quickSort(a,0 , len-1);
-        for(i = 0 ; i < len ; i++)
-            System.out.print(a[i]+" ");
-        System.out.println(a.length);
+    public static void main(String[] args) {
+        int a[] = {9, 0, 2, 3, 1};
+        System.out.println(Arrays.toString(a));
+        quickSort(a, 0, a.length - 1);
+        System.out.println(Arrays.toString(a));
     }
+
+
+//    private void quickSort(int left, int right, int[] nums) {
+//        if (left >= right)
+//            return;
+//        int i = left, j = right, pivot = nums[left];
+//        while (i < j) {
+//            while (i < j && nums[j] >= pivot)
+//                j--;
+//            if (i < j)
+//                nums[i] = nums[j];
+//            while (i < j && nums[i] <= pivot)
+//                i++;
+//            if (i < j)
+//                nums[j] = nums[i];
+//        }
+//        nums[i] = pivot;
+//        quickSort(left, i - 1, nums);
+//        quickSort(i + 1, right, nums);
+//    }
+
+//    public void quickSort(int array[], int low, int high) {
+//        int i, j;
+//        int index;
+//        if (low >= high) return;
+//
+//        i = low;
+//        j = high;
+//        //stash一下最左边的元素
+//        index = array[i];
+//        while (i < j) {
+//            while (i < j && array[j] > index)
+//                j--;
+//            if (i < j)
+//                array[i++] = array[j];
+//            while (i < j && array[i] < index)
+//                i++;
+//            if (i < j)
+//                array[j--] = array[i];
+//        }
+//        array[i] = index;
+//        quickSort(array, low, i - 1);
+//        quickSort(array, i + 1, high);
+//
+//    }
 
 }
