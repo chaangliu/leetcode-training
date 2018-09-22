9176bad3404900b00b4b4e6e4e17797a247e0ca6
chaangliu <chaangliu@tencent.com>
2018-09-22 19:29:15 +0800

    combination sum ii

--- dfs/CombinationSum.java
+++ dfs/CombinationSum.java
@@ -1,6 +1,7 @@
 package dfs;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -22,66 +23,72 @@ import java.util.List;
  */
 
 public class CombinationSum {
+    //############# 2018-09-22 Review #############
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (candidates == null || candidates.length == 0) {
+            return res;
+        }
+        Arrays.sort(candidates);//要点0. 要先sort
+        backtrack(candidates, target, res, new ArrayList<Integer>(), 0);
+        return res;
+    }
 
+    //#my version:
+    // (我觉得下次还是不要用有返回值的backtrack比较清晰)
+    private List<List<Integer>> backtracking(int[] candidates, int target, List<List<Integer>> res, List<Integer> item, int start) {
+        for (int i = start; i < candidates.length; i++) {//要点1. 重复利用一个数，不代表可以用之前的数
+            if (target < candidates[i]) return res;
+            item.add(candidates[i]);//要点2. 这次写的时候判断了candidates[i] == target，导致无论candidates[i] == target是否成立，都要先add candidate。写成target == 0比较好。
+            if (candidates[i] == target) {
+                res.add(new ArrayList<Integer>(item));
+                item.remove(item.size() - 1);//要点3. candidates[i] == target，要额外add和remove
+                return res;
+            }
+            backtracking(candidates, target - candidates[i], res, item, i);
+            item.remove(item.size() - 1);
+        }
+        return res;
+    }
 
-	List<List<Integer>> result = new ArrayList<>();
+    //good version:
+    private void backtrack(int[] nums, int remain, List<List<Integer>> list, List<Integer> tempList, int start) {
+        if (remain < 0) return;
+        if (remain == 0) {
+            list.add(new ArrayList<>(tempList));
+            return;
+        }
+        for (int i = start; i < nums.length; i++) {
+            //加上下面这句对结果没有影响，但会减少很多次循环，因为同一个数字可以复用（每次从i开始），所以重复数字就没有意义了
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+            tempList.add(nums[i]);
+            backtrack(nums, remain - nums[i], list, tempList, i); // not i + 1 because we can reuse same elements
+            tempList.remove(tempList.size() - 1);
+        }
+    }
 
-	public List<List<Integer>> combinationSum(int[] candidates, int target) {
-
-		if (candidates == null || candidates.length == 0) return result;
-		dfs(target, new ArrayList<Integer>(), candidates, 0);
-		return result;
-	}
-
-	private void dfs(int target, List<Integer> item, int[] candidates, int start) {
-
-		if (target < 0) return;
-		if (target == 0) {
-			result.add(new ArrayList<>(item));
-			return;
-		}
-		for (int i = start; i < candidates.length; i++) {
-			//加上下面这句对结果没有影响，但会减少很多次循环，因为同一个数字可以复用（每次从i开始），所以重复数字就没有意义了
-			if (i > 0 && candidates[i] == candidates[i - 1]) continue;
-			item.add(candidates[i]);
-			dfs(target - candidates[i], item, candidates, i);
-			item.remove(item.size() - 1);
-		}
-	}
-
-
-	public static void main(String args[]) {
-		CombinationSum combinationSum = new CombinationSum();
-		int[] candidates = new int[]{8, 2, 3, 6};
-		combinationSum.combinationSum(candidates, 7);
-		System.out.println(combinationSum.result);
-
-	}
-
-
-//    private List<List<Integer>> result = new ArrayList<>();
+//	List<List<Integer>> result = new ArrayList<>();
 //
-//    private List<List<Integer>> combinationSum(int[] candidates, int target) {
-//        Arrays.sort(candidates);
-//        dfs(candidates, 0, new ArrayList<Integer>(), result, target);
-//        return result;
-//    }
+//	public List<List<Integer>> combinationSum(int[] candidates, int target) {
 //
-//    private void dfs(int[] candidates, int start, List<Integer> cell, List<List<Integer>> result, int target) {
-//        if (target == 0) {
-//            result.add(new ArrayList<>(cell));
-//            return;
-//        }
-//        for (int i = start; i < candidates.length; i++) {
-//            //加上下面这句对结果没有影响，但会减少很多次循环，因为同一个数字可以复用（每次从i开始），所以有重复数字就没必要了
-//            if (i > 0 && candidates[i] == candidates[i - 1]) continue;
-//            if (target > 0) {
-//                cell.add(candidates[i]);
-//                dfs(candidates, i+1, cell, result, target - candidates[i]);
-//                cell.remove(cell.size() - 1);
-//            } else return;
-//        }
-//    }
-
-
+//		if (candidates == null || candidates.length == 0) return result;
+//		dfs(target, new ArrayList<Integer>(), candidates, 0);
+//		return result;
+//	}
+//
+//	private void dfs(int target, List<Integer> item, int[] candidates, int start) {
+//
+//		if (target < 0) return;
+//		if (target == 0) {
+//			result.add(new ArrayList<>(item));
+//			return;
+//		}
+//		for (int i = start; i < candidates.length; i++) {
+//			//加上下面这句对结果没有影响，但会减少很多次循环，因为同一个数字可以复用（每次从i开始），所以重复数字就没有意义了
+//			if (i > 0 && candidates[i] == candidates[i - 1]) continue;
+//			item.add(candidates[i]);
+//			dfs(target - candidates[i], item, candidates, i);
+//			item.remove(item.size() - 1);
+//		}
+//	}
 }
--- dfs/CombinationSum2.java
+++ dfs/CombinationSum2.java
@@ -24,38 +24,68 @@ import java.util.List;
  */
 
 public class CombinationSum2 {
-    List<List<Integer>> result = new ArrayList<>();
-    int[] mCandidates = {};
 
+    //############# 2018-09-22 Review #############
+    //这题有必要再多做几遍啊
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
-        this.mCandidates = candidates;
-        Arrays.sort(mCandidates);
-        dfs(new ArrayList<Integer>(), target, 0);
-        return result;
+        List<List<Integer>> res = new ArrayList<>();
+        if (candidates == null || candidates.length == 0) {
+            return res;
+        }
+        Arrays.sort(candidates);
+        backtrack(res, new ArrayList<Integer>(), candidates, target, 0);
+        return res;
     }
 
-    public void dfs(List<Integer> currentList, int target, int startPoint) {
-        ArrayList<Integer> newList = new ArrayList<>(currentList);
-        if (target == 0) {
-            result.add(newList);
+    private void backtrack(List<List<Integer>> res, List<Integer> item, int[] candidates, int remain, int start) {
+        if (remain < 0)
+            return; //已犯错误: 写成了remain < candidates[i]， 2. start = i + 1的情况，if语句一定要放for的外面，不然target == candidates[len -1 ]的case会漏掉
+        if (remain == 0) {
+            res.add(new ArrayList<Integer>(item));
             return;
         }
-        for (int i = startPoint; i < mCandidates.length && mCandidates[i] <= target; i++) {
-            currentList.add(mCandidates[i]);
-            dfs(currentList, target - mCandidates[i], i + 1);
-            currentList.remove(Integer.valueOf(mCandidates[i]));
-            while (i < mCandidates.length - 1 && mCandidates[i] == mCandidates[i + 1]) {
-//                continue;
-                i++;
-            }
+        for (int i = start; i < candidates.length; i++) {
+            if (i > 0 && candidates[i] == candidates[i - 1] && i != start)//已犯错误：这里尤其要注意，i != start(或者i > start)才代表这个数应该跳过，而不是i == start。有一定思维难度，调试才能看出。
+                continue;
+            item.add(candidates[i]);
+            backtrack(res, item, candidates, remain - candidates[i], i + 1);
+            item.remove(item.size() - 1);
         }
     }
 
-    public static void main(String args[]) {
-        CombinationSum2 combinationSum2 = new CombinationSum2();
-        int[] candidates = new int[]{10, 1, 2, 7, 6, 1, 5};
-        combinationSum2.combinationSum2(candidates, 7);
-        System.out.println(combinationSum2.result);
+    //############# 2017/1/2 #############
+//    List<List<Integer>> result = new ArrayList<>();
+//    int[] mCandidates = {};
+//
+//    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+//        this.mCandidates = candidates;
+//        Arrays.sort(mCandidates);
+//        dfs(new ArrayList<Integer>(), target, 0);
+//        return result;
+//    }
+//
+//    public void dfs(List<Integer> currentList, int target, int startPoint) {
+//        ArrayList<Integer> newList = new ArrayList<>(currentList);
+//        if (target == 0) {
+//            result.add(newList);
+//            return;
+//        }
+//        for (int i = startPoint; i < mCandidates.length && mCandidates[i] <= target; i++) {
+//            currentList.add(mCandidates[i]);
+//            dfs(currentList, target - mCandidates[i], i + 1);
+//            currentList.remove(Integer.valueOf(mCandidates[i]));
+//            while (i < mCandidates.length - 1 && mCandidates[i] == mCandidates[i + 1]) {
+////                continue;
+//                i++;
+//            }
+//        }
+//    }
 
-    }
+//    public static void main(String args[]) {
+//        CombinationSum2 combinationSum2 = new CombinationSum2();
+//        int[] candidates = new int[]{10, 1, 2, 7, 6, 1, 5};
+//        combinationSum2.combinationSum2(candidates, 7);
+//        System.out.println(combinationSum2.result);
+//
+//    }
 }
