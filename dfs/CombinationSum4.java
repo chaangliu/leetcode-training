2b4582b5610499c03d794f73cca37f76287a0c8d
liuchang <chaangliu@tencent.com>
2018-09-24 12:19:09 +0800

    CombinationSum4

--- dfs/CombinationSum4.java
+++ dfs/CombinationSum4.java
@@ -1,6 +1,7 @@
 package dfs;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -33,9 +34,29 @@ import java.util.List;
  */
 public class CombinationSum4 {
 
-    //todo Dynamic Programing
+    //# DP,
+    //Imagine we only need one more number to reach target, this number can be any one in the array.
+    //comb[target] = sum(comb[target - nums[i]]), where 0 <= i < nums.length, and target >= nums[i].
     public int combinationSum4(int[] nums, int target) {
-        return 0;
+        if (nums == null || nums.length == 0 || target < 1) return 0;
+        int[] dp = new int[target + 1];
+        Arrays.fill(dp, -1);
+        dp[0] = 1;//In how many ways is it possible to get zero count given that all the elements are positive?" One 1 way - to not choose any element n choose 0
+        return helper(dp, nums, target);
+    }
+
+    private int helper(int[] dp, int[] nums, int target) {
+        if (dp[target] != -1) {// not dp[i]
+            return dp[target];
+        }
+        int res = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (target >= nums[i]) {
+                res += helper(dp, nums, target - nums[i]);
+            }
+        }
+        dp[target] = res;
+        return res;
     }
 
 
