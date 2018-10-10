7aa7cd7170dd4bd22fa1dea2f9b8202e4f73866b
liuchang <chaangliu@tencent.com>
2018-10-10 21:31:16 +0800

    //Approach 1. 先交换，再递归执行子树

--- easy/InvertBinaryTree.java
+++ easy/InvertBinaryTree.java
@@ -16,10 +16,11 @@ package easy;
  */
 
 public class InvertBinaryTree {
+
+	//20181010 review
+	//Approach 1. 自顶向下。先交换，再递归执行子树。
 	public TreeNode invertTree(TreeNode root) {
-		if (root == null) {
-			return null;
-		}
+		if(root == null) return null;
 		TreeNode tmp = root.left;
 		root.left = root.right;
 		root.right = tmp;
@@ -27,4 +28,27 @@ public class InvertBinaryTree {
 		invertTree(root.right);
 		return root;
 	}
+
+	//Approach 2. 自底向上。
+//	public TreeNode invertTree(TreeNode root) {
+//		if(root == null) return null;
+//		TreeNode left = invertTree(root.right);//新的左子树就是翻转后的root.right
+//		TreeNode right = invertTree(root.left);
+//		root.left = left;
+//		root.right = right;
+//		return root;
+//	}
+
+	//original
+//	public TreeNode invertTree(TreeNode root) {
+//		if (root == null) {
+//			return null;
+//		}
+//		TreeNode tmp = root.left;
+//		root.left = root.right;
+//		root.right = tmp;
+//		invertTree(root.left);
+//		invertTree(root.right);
+//		return root;
+//	}
 }
