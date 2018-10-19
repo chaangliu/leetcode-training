c47412228884ecb746e3ce0cb1a1a1ee920b5c0c
chaangliu <chaangliu@tencent.com>
2018-10-19 14:23:27 +0800

    kth smallest element in a bst

--- array/KthSmallestElementInaBST.java
+++ array/KthSmallestElementInaBST.java
@@ -9,53 +9,83 @@ import tree.TreeNode;
  */
 
 public class KthSmallestElementInaBST {
-	public int kthSmallest1(TreeNode root, int k) {
-		Stack<TreeNode> stack = new Stack<>();
-		while (root != null || !stack.isEmpty()) {
-			while (root != null) {
-				stack.push(root);
-				root = root.left;
-			}
-			root = stack.pop();
+
+
+    //20181019 review. 思路就是BST的遍历。用traversal或者stack
+    public int kthSmallest3(TreeNode root, int k) {
+        Stack<TreeNode> stack = new Stack<>();
+        while (root != null || !stack.empty()) {
+            while (root != null) {
+                stack.push(root);
+                root = root.left;
+            }
+            root = stack.pop();
+            if (--k == 0) {//是--k，不是k--，一年多了犯了同样的错误？？。。
+                return root.val;
+            }
+            root = root.right;
+        }
+        return -1;
+    }
+
+    //original thread
+    public int kthSmallest1(TreeNode root, int k) {
+        Stack<TreeNode> stack = new Stack<>();
+        while (root != null || !stack.isEmpty()) {
+            while (root != null) {
+                stack.push(root);
+                root = root.left;
+            }
+            root = stack.pop();
 //            k--;
-			if (k-- == 0) return root.val;
-			root = root.right;
-		}
-		return -1;
-	}
+            if (k-- == 0) return root.val;//xx => --k
+            root = root.right;
+        }
+        return -1;
+    }
 
-	int res, count;
+    int res, count;
 
-	public int kthSmallest2(TreeNode root, int k) {
-		count = k;
-		recursion(root);
-		return res;
-	}
+    public int kthSmallest2(TreeNode root, int k) {
+        count = k;
+        recursion(root);
+        return res;
+    }
 
-	private void recursion(TreeNode root) {
+    private void recursion(TreeNode root) {
 
-		if (root == null) return;
-		recursion(root.left);
-		count--;
-		if (count == 0) {
-			res = root.val;
-		}
-		recursion(root.right);
-	}
+        if (root == null) return;
+        recursion(root.left);
+        count--;
+        if (count == 0) {
+            res = root.val;
+            return;
+        }
+        recursion(root.right);
+    }
 
 
-	public int kthSmallest(TreeNode root, int k) {
-		int left = countNode(root.left);
-		if (left + 1 == k) return root.val;
-		else if (left + 1 > k) return kthSmallest(root.left, k);
-		else return kthSmallest(root.right, k - left - 1);
-	}
+    public int kthSmallest(TreeNode root, int k) {
+        int left = countNode(root.left);
+        if (left + 1 == k) return root.val;
+        else if (left + 1 > k) return kthSmallest(root.left, k);
+        else return kthSmallest(root.right, k - left - 1);
+    }
 
-	private int countNode(TreeNode root) {
-		//这里return 的应该是0 。用root只有一个node的情形考虑就行了。
-		if (root == null) return 0;
-		return 1 + countNode(root.left) + countNode(root.right);
-	}
+    private int countNode(TreeNode root) {
+        //这里return 的应该是0 。用root只有一个node的情形考虑就行了。
+        if (root == null) return 0;
+        return 1 + countNode(root.left) + countNode(root.right);
+    }
+
+    public static void main(String args[]) {
+        TreeNode node = new TreeNode(3);
+        node.left = new TreeNode(1);
+        node.right = new TreeNode(4);
+        node.left.right = new TreeNode(2);
+        int k = new KthSmallestElementInaBST().kthSmallest3(node, 1);
+        System.out.println(k);
+    }
 
 //
 //	public boolean judgeCircle(String moves) {
@@ -79,5 +109,4 @@ public class KthSmallestElementInaBST {
 //		}
 //		return up == down && left == right;
 //	}
-
 }
