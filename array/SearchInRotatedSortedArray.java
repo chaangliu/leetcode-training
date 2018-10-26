1d67fbc336243881b7ac3fdb16fa6cfe9738e0fc
liuchang <chaangliu@tencent.com>
2018-10-26 23:59:46 +0800

    search in rotated sorted arrary

--- array/SearchInRotatedSortedArray.java
+++ array/SearchInRotatedSortedArray.java
@@ -10,7 +10,7 @@ package array;
 
 public class SearchInRotatedSortedArray {
 
-//    public static int search(int nums[], int target) {
+    //    public static int search(int nums[], int target) {
 //        int len = nums.length;
 //        int left = 0, right = len - 1;
 //        while (left <= right) {
@@ -34,28 +34,46 @@ public class SearchInRotatedSortedArray {
 //        }
 //        return -1;
 //    }
+//    public static int search(int nums[], int target) {
+//        int len = nums.length;
+//        int left = 0, right = len - 1;
+//        while (left <= right) {
+//            int mid = (left + right) / 2;
+//            if (nums[mid] == target)
+//                return mid;
+//            if (nums[mid] >= nums[left]) {
+//                if (target >= nums[left] && target < nums[mid])
+//                    right = mid - 1;
+//                else left = mid + 1;
+//            } else {
+//                if (target > nums[mid] && target <= nums[right])
+//                    left = mid + 1;
+//                else right = mid - 1;
+//            }
+//        }
+//        return -1;
+//    }
+
     public static int search(int nums[], int target) {
         int len = nums.length;
         int left = 0, right = len - 1;
         while (left <= right) {
-            int mid = (left + right) / 2;
-            if (nums[mid] == target)
-				return mid;
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) return mid;
             if (nums[mid] >= nums[left]) {
-                if (target >= nums[left] && target < nums[mid])
+                if (target >= nums[left] && target < nums[mid]) {
                     right = mid - 1;
-                else left = mid + 1;
+                } else left = mid + 1;
             } else {
-                if (target > nums[mid] && target <= nums[right])
+                if (target > nums[mid] && target < nums[right])
                     left = mid + 1;
                 else right = mid - 1;
             }
         }
-        return -1;
     }
 
-    public static void main(String args[]){
-        int a[] = {3,1};
-        System.out.println(search(a,1));
+    public static void main(String args[]) {
+        int a[] = {3, 1};
+        System.out.println(search(a, 1));
     }
 }
