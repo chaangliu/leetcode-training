3f59126a7f6865fefd9190d514d0868c19861f1d
chaangliu <chaangliu@tencent.com>
2018-11-13 16:04:07 +0800

    copy random list node

---
+++ jianzhioffer/CopyRandomListNode.java
@@ -0,0 +1,77 @@
+package jianzhioffer;
+
+/**
+ * 输入一个复杂链表（每个节点中有节点值，以及两个指针，一个指向下一个节点，另一个特殊指针指向任意一个节点），返回结果为复制后复杂链表的head。
+ * （注意，输出结果中请不要返回参数中的节点引用，否则判题程序会直接返回空）
+ */
+
+
+class RandomListNode {
+    int label;
+    RandomListNode next = null;
+    RandomListNode random = null;
+
+    RandomListNode(int label) {
+        this.label = label;
+    }
+}
+
+public class CopyRandomListNode {
+    //第一步，在旧链表中创建新链表，此时不处理新链表的next和random（因为random指向的节点可能还没初始化出来）
+    //第二步，初始化新链表的random
+    //第三步，分离两个链表
+    public RandomListNode Clone(RandomListNode pHead) {
+        if (pHead == null) return null;
+        RandomListNode node = pHead;
+
+        //1. A1 -> A1' -> A2 -> A2'
+        while (node != null) {
+            RandomListNode tmp = node.next;
+            node.next = new RandomListNode(node.label);
+            node.next.next = tmp;
+            node = tmp;
+        }
+
+        //2. 初始化random
+        node = pHead;
+        while (node != null) {
+            if (node.random != null) {//易错
+                node.next.random = node.random.next;
+            }
+            node = node.next.next;
+        }
+
+        //3. 分离
+//        node = pHead.next;
+//        RandomListNode fakeHead = new RandomListNode(-1);
+//        fakeHead.next = node;
+//        while (node != null && node.next != null) {
+//            node.next = node.next.next;
+//            node = node.next;//易错
+//        }
+//        return fakeHead.next;
+        node = pHead;
+        RandomListNode pCloneHead = pHead.next;
+        while (node != null) {
+            RandomListNode cloneNode = node.next;
+            node.next = cloneNode.next;
+            cloneNode.next = cloneNode.next == null ? null : cloneNode.next.next;
+            node = node.next;
+        }
+        return pCloneHead;
+    }
+
+    public static void main(String args[]) {
+        RandomListNode node1 = new RandomListNode(1);
+        RandomListNode node2 = new RandomListNode(2);
+        RandomListNode node3 = new RandomListNode(3);
+
+        node1.next = node2;
+        node1.random = node3;
+        node2.next = node3;
+        node2.random = node1;
+        node3.random = node1;
+
+        new CopyRandomListNode().Clone(node1);
+    }
+}
--- jianzhioffer/FindPathInTree.java
+++ jianzhioffer/FindPathInTree.java
@@ -6,9 +6,12 @@ import java.util.Comparator;
 import java.util.List;
 
 /**
- * 本题类似BinaryTreePaths
+ * 链接：https://www.nowcoder.com/questionTerminal/b736e784e3e34731af99065031301bca
+ * 输入一颗二叉树的跟节点和一个整数，打印出二叉树中结点值的和为输入整数的所有路径。路径定义为从树的根结点开始往下一直到叶结点所经过的结点形成一条路径。(注意: 在返回值的list中，数组长度大的数组靠前)
  */
 public class FindPathInTree {
+
+    ///我的答案，这题跟BinaryTreePaths和CombinationSum的解法类似。需要注意的就是target的判断和不要多remove一次。
     public ArrayList<ArrayList<Integer>> FindPath(TreeNode root, int target) {
         ArrayList<ArrayList<Integer>> res = new ArrayList<>();
         if (root == null) return res;
@@ -47,5 +50,21 @@ public class FindPathInTree {
         root.left.left = new TreeNode(4);
         root.left.right = new TreeNode(7);
         new FindPathInTree().FindPath(root, 22);
+    }
+
+    ///牛客的答案
+    private ArrayList<ArrayList<Integer>> listAll = new ArrayList<ArrayList<Integer>>();
+    private ArrayList<Integer> list = new ArrayList<Integer>();
+
+    public ArrayList<ArrayList<Integer>> FindPath2(TreeNode root, int target) {
+        if (root == null) return listAll;
+        list.add(root.val);
+        target -= root.val;
+        if (target == 0 && root.left == null && root.right == null)
+            listAll.add(new ArrayList<Integer>(list));
+        FindPath(root.left, target);
+        FindPath(root.right, target);
+        list.remove(list.size() - 1);
+        return listAll;
     }
 }
