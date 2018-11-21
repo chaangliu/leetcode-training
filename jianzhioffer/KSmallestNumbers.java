e0dc67088bc21a31136563968b5d519be9d06716
liuchang <chaangliu@tencent.com>
2018-11-21 23:23:16 +0800

    max heap

---
+++ jianzhioffer/KSmallestNumbers.java
@@ -0,0 +1,43 @@
+package jianzhioffer;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+
+/**
+ * 输入n个整数，找出其中最小的K个数。例如输入4,5,1,6,2,7,3,8这8个数字，则最小的4个数字是1,2,3,4,。
+ */
+public class KSmallestNumbers {
+    //这一题非常好，把priorityQueue和最大最小堆的关系体现了出来。
+    //用最大堆保存这k个数，每次只和堆顶比，如果比堆顶小，删除堆顶，新数入堆。
+    //遍历、建堆，复杂度是O(n + nlogk)
+    public ArrayList<Integer> GetLeastNumbers_Solution(int[] input, int k) {
+        ArrayList<Integer> res = new ArrayList<>();
+        if (input == null || input.length == 0 || k > input.length || k <= 0) return res;
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return o2 - o1;
+            }
+        });
+
+        for (int i = 0; i < input.length; i++) {
+            if (maxHeap.size() < k) {
+                maxHeap.offer(input[i]);
+            } else {
+                if (maxHeap.peek() > input[i]) {
+                    maxHeap.poll();
+                    maxHeap.offer(input[i]);
+                }
+            }
+        }
+        res.addAll(maxHeap);
+        return res;
+    }
+
+    public static void main(String args[]) {
+        int[] nums = {4, 5, 1, 6, 2, 7, 3, 8};
+        new KSmallestNumbers().GetLeastNumbers_Solution(nums, 4);
+    }
+}
