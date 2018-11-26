c06dd64c382eb647f3c2ad04d3d3531605fdd846
liuchang <chaangliu@tencent.com>
2018-11-26 23:42:16 +0800

    minimum increment to make array unique

--- array/MinimumIncrementToMakeArrayUnique.java
+++ array/MinimumIncrementToMakeArrayUnique.java
@@ -25,7 +25,7 @@ import java.util.Set;
 public class MinimumIncrementToMakeArrayUnique {
 
     //brute force, TLE
-    public int minIncrementForUnique(int[] A) {
+    public int minIncrementForUniqueTLE(int[] A) {
         if (A == null || A.length == 0) return 0;
         int res = 0;
         Set<Integer> set = new HashSet<>();
@@ -44,9 +44,33 @@ public class MinimumIncrementToMakeArrayUnique {
         return res;
     }
 
+    //solutions里的解法，记录先取走，遇到合适的位置就填充
+    public int minIncrementForUnique(int[] A) {
+        if (A == null || A.length == 0) return 0;
+        //题目说数字不大于100000
+        int[] map = new int[100000];
+        //计算每个数字出现次数
+        for (int i = 0; i < A.length; i++) {
+            map[A[i]]++;
+        }
+        int ans = 0, taken = 0;
+        for (int i = 0; i < 100000; i++) {
+            if (map[i] > 1) {
+                //拿走n-1个重复的数字
+                taken += map[i] - 1;
+                //提前减去n个对应的数，比如1,1,1,1,3,5，就提前减去3个1
+                ans -= i * (map[i] - 1);
+            } else if (map[i] == 0 && taken > 0) {
+                taken--;
+                ans += i;
+            }
+        }
+        return ans;
+    }
+
     public static void main(String args[]) {
-//        int nums[] = {1, 2, 2};
-        int nums[] = {3,2,1,2,1,7};
+        int nums[] = {1, 2, 2};
+//        int nums[] = {3, 2, 1, 2, 1, 7};
         new MinimumIncrementToMakeArrayUnique().minIncrementForUnique(nums);
     }
 }
