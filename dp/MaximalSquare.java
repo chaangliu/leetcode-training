48139dee045c825d6a4417a1c971405e1c157447
chaangliu <chaangliu@tencent.com>
2019-01-21 19:28:27 +0800

    221. Maximal Square - one dimensional dp

--- dp/MaximalSquare.java
+++ dp/MaximalSquare.java
@@ -40,4 +40,27 @@ public class MaximalSquare {
         }
         return maxsqlen * maxsqlen;
     }
+
+    //approach 3. 1dimensional dp；滚动数组
+    //动态转移方程：dp(i)=min(prev, dp(i−1), dp(i)) + 1.
+    public int maximalSquare_______1d(char[][] matrix) {
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) return 0;
+        int rows = matrix.length, cols = matrix[0].length;
+        int[] dp = new int[cols + 1];
+        int maxsqlen = 0;
+        int prev = 0;
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= cols; j++) {
+                int temp = dp[j];/// temp 是老的dp[j - 1]，也就是左上角的格子（而不是左边的格子）
+                if (matrix[i - 1][j - 1] == '1') {
+                    dp[j] = Math.min(Math.min(dp[j], dp[j - 1]), prev) + 1;//dp[j - 1]是左边的格子，因为是上次dp的结果
+                    maxsqlen = Math.max(maxsqlen, dp[j]);
+                } else {
+                    dp[j] = 0; //易漏
+                }
+                prev = temp;
+            }
+        }
+        return maxsqlen * maxsqlen;
+    }
 }
