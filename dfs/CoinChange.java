c30bc1537ad3fe2326b8c837ff9a4a9fb08b6eae
chaangliu <chaangliu@tencent.com>
2019-01-29 18:51:50 +0800

    322. Coin Change

--- dfs/CoinChange.java
+++ dfs/CoinChange.java
@@ -46,27 +46,65 @@ public class CoinChange {
 //        }
 //    }
 
-    //1. brute force
-    public int coinChange(int[] coins, int amount) {
-        return coinChange(0, coins, amount);
-    }
+//    //1. brute force (dfs)
+//    public int coinChange(int[] coins, int amount) {
+//        return coinChange(0, coins, amount);
+//    }
+//
+//    private int coinChange(int idxCoin, int[] coins, int amount) {
+//        if (amount == 0)
+//            return 0;
+//        if (idxCoin < coins.length && amount > 0) {
+//            int maxVal = amount / coins[idxCoin];
+//            int minCost = Integer.MAX_VALUE;
+//            for (int x = 0; x <= maxVal; x++) {
+//                if (amount >= x * coins[idxCoin]) {
+//                    int res = coinChange(idxCoin + 1, coins, amount - x * coins[idxCoin]);
+//                    if (res != -1)
+//                        minCost = Math.min(minCost, res + x);
+//                }
+//            }
+//            return (minCost == Integer.MAX_VALUE) ? -1 : minCost;
+//        }
+//        return -1;
+//    }
 
-    private int coinChange(int idxCoin, int[] coins, int amount) {
-        if (amount == 0)
-            return 0;
-        if (idxCoin < coins.length && amount > 0) {
-            int maxVal = amount / coins[idxCoin];
-            int minCost = Integer.MAX_VALUE;
-            for (int x = 0; x <= maxVal; x++) {
-                if (amount >= x * coins[idxCoin]) {
-                    int res = coinChange(idxCoin + 1, coins, amount - x * coins[idxCoin]);
-                    if (res != -1)
-                        minCost = Math.min(minCost, res + x);
-                }
+    //2. dfs with cache, top down, code from leetcode solutions
+//    public class Solution {
+//
+//        public int coinChange(int[] coins, int amount) {
+//            if (amount < 1) return 0;
+//            return coinChange(coins, amount, new int[amount]);
+//        }
+//
+//        private int coinChange(int[] coins, int rem, int[] count) {
+//            if (rem < 0) return -1;
+//            if (rem == 0) return 0;
+//            if (count[rem - 1] != 0) return count[rem - 1];
+//            int min = Integer.MAX_VALUE;
+//            for (int coin : coins) {
+//                int res = coinChange(coins, rem - coin, count);
+//                if (res >= 0 && res < min)
+//                    min = 1 + res;
+//            }
+//            count[rem - 1] = (min == Integer.MAX_VALUE) ? -1 : min;
+//            return count[rem - 1];
+//        }
+//    }
+
+    //3. dp, bottom up
+    public int coinChange(int[] coins, int amount) {
+        if (coins == null || coins.length == 0 || amount < 0) return -1;
+        if (amount == 0) return 0;
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp, Integer.MAX_VALUE - 1);
+        dp[0] = 0;
+        for (int i = 1; i <= amount; i++)
+            for (int j = 0; j < coins.length; j++) {
+                if (i - coins[j] >= 0)
+                    dp[i] = Math.min(dp[i], dp[i - coins[j]] + 1);//这里是滚动数组，二维转一维；另外这里隐含了一个条件，如果已经计算出dp[i - coins[j]]的话才加以利用
             }
-            return (minCost == Integer.MAX_VALUE) ? -1 : minCost;
-        }
-        return -1;
+        return dp[amount] == Integer.MAX_VALUE - 1 ? -1 : dp[amount];
     }
 
     public static void main(String args[]) {
