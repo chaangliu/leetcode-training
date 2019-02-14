4a0bc320b357fdc2d1bc0f697d5d07a26848132b
chaangliu <chaangliu@tencent.com>
2019-02-14 09:59:10 +0800

    279. Perfect Squares -- bfs

--- dp/PerfectSquares.java
+++ dp/PerfectSquares.java
@@ -1,6 +1,8 @@
 package dp;
 
 import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
 
 /**
  * Given a positive integer n, find the least number of perfect square numbers (for example, 1, 4, 9, 16, ...) which sum to n.
@@ -22,7 +24,7 @@ public class PerfectSquares {
 
     //1. dp
     //状态转移方程： //dp[i] = min(dp[i - j * j] + 1)
-    public int numSquares(int n) {
+    public int numSquares___DP(int n) {
         if (n <= 0) return 0;
         int[] dp = new int[n + 1];
         Arrays.fill(dp, Integer.MAX_VALUE);
@@ -35,8 +37,32 @@ public class PerfectSquares {
         return dp[n];
     }
 
-    //另外，这题还可以用BFS和MATH解法
-    //https://leetcode.com/problems/perfect-squares/discuss/71488/Summary-of-4-different-solutions-(BFS-DP-static-DP-and-mathematics)
+    //https://leetcode.com/problems/perfect-squares/discuss/71475/Short-Python-solution-using-BFS
+    //2. bfs解法，想象成树的bfs，res是最早node.val是0的那个level
+    public int numSquares___BFS(int n) {
+        if (n <= 0) return 0;
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(n);
+        int level = 1;
+        int curNum = 1, nextNum = 0;
+        while (!queue.isEmpty()) {
+            int remain = queue.poll();
+            curNum--;
+            for (int i = 1; i * i <= remain; i++) {//已犯错误1：<=, NOT <
+                if (remain - i * i == 0) return level;//已犯错误2：remain - i * i, NOT n - i * i
+                queue.add(remain - i * i);
+                nextNum++;
+            }
+            if (curNum == 0) {
+                level++;
+                curNum = nextNum;
+                nextNum = 0;
+            }
+        }
+        return -1;
+    }
 
-
+    public static void main(String args[]) {
+        System.out.println(new PerfectSquares().numSquares___BFS(13));
+    }
 }
