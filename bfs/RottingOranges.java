14aed9aeca05b99c002f8ccf4ea6672ad819ad8c
chaangliu <chaangliu@tencent.com>
2019-02-17 12:48:59 +0800

    994. Rotting Oranges

---
+++ bfs/RottingOranges.java
@@ -0,0 +1,85 @@
+package bfs;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * In a given grid, each cell can have one of three values:
+ * <p>
+ * the value 0 representing an empty cell;
+ * the value 1 representing a fresh orange;
+ * the value 2 representing a rotten orange.
+ * Every minute, any fresh orange that is adjacent (4-directionally) to a rotten orange becomes rotten.
+ * <p>
+ * Return the minimum number of minutes that must elapse until no cell has a fresh orange.  If this is impossible, return -1 instead.
+ * <p>
+ * Example 1:
+ * <p>
+ * <p>
+ * <p>
+ * Input: [[2,1,1],[1,1,0],[0,1,1]]
+ * Output: 4
+ * Example 2:
+ * <p>
+ * Input: [[2,1,1],[0,1,1],[1,0,1]]
+ * Output: -1
+ * Explanation:  The orange in the bottom left corner (row 2, column 0) is never rotten, because rotting only happens 4-directionally.
+ * Example 3:
+ * <p>
+ * Input: [[0,2]]
+ * Output: 0
+ * Explanation:  Since there are already no fresh oranges at minute 0, the answer is just 0.
+ * <p>
+ * <p>
+ * 20190217weekly contest
+ */
+public class RottingOranges {
+    /**
+     * 此题在群内WanYing等朋友的指导下才AC（感觉这题标easy是因为queue bfs这种题大家都已经做烂了，默认是基本功了）
+     * 我一开始想着每次for循环遇到的2都加入到queue，然后再维持一个全局最小的时间；这样的问题是，
+     * 没法做到2-end/multi-end bfs，比如左下角和右下角同时开始rot，这样速度是大大缩短的；而且就算用我那种方案实施起来，还要把grid的状态还原回去
+     * 对于2-end bfs，我一开始理解的bfs是queue里一开始只能有一个元素；但是仔细想一下，每次while代表一次bfs的扩张，那么一开始queue中有k个元素的话，就相当于从k个起点一开开始bfs了；类似的思路OpenTheLock那题有2-end bfs
+     */
+    public int orangesRotting(int[][] grid) {
+        if (grid == null || grid.length == 0 || grid[0].length == 0) return 0;
+        Queue<int[]> queue = new LinkedList<>();
+        int row = grid.length;
+        int col = grid[0].length;
+        int res = -1;
+        for (int i = 0; i < row; i++)
+            for (int j = 0; j < col; j++) {
+                if (grid[i][j] == 2) {
+                    queue.offer(new int[]{i, j});
+                }
+            }
+        while (!queue.isEmpty()) {
+            res++;
+            int size = queue.size();
+            for (int k = 0; k < size; k++) {
+                int[] cord = queue.poll();
+                int x = cord[0], y = cord[1];
+                if (x - 1 >= 0 && grid[x - 1][y] == 1) {
+                    grid[x - 1][y] = 2;//已犯错误：忘记更新已经visit的状态
+                    queue.offer(new int[]{x - 1, y});
+                }
+                if (x + 1 < row && grid[x + 1][y] == 1) {
+                    grid[x + 1][y] = 2;
+                    queue.offer(new int[]{x + 1, y});
+                }
+                if (y - 1 >= 0 && grid[x][y - 1] == 1) {
+                    grid[x][y - 1] = 2;
+                    queue.offer(new int[]{x, y - 1});
+                }
+                if (y + 1 < col && grid[x][y + 1] == 1) {
+                    grid[x][y + 1] = 2;
+                    queue.offer(new int[]{x, y + 1});
+                }
+            }
+        }
+        for (int[] r : grid)
+            for (int v : r)
+                if (v == 1)
+                    return -1;
+        return res == -1 ? 0 : res;
+    }
+}
