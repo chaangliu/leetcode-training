193312a8f2800b036772c87cfafa9693d45c6c4a
chaangliu <chaangliu@tencent.com>
2019-02-22 00:10:39 +0800

    225. Implement Stack using Queues

---
+++ design/ImplementStackUsingQueues.java
@@ -0,0 +1,74 @@
+package design;
+
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * Implement the following operations of a stack using queues.
+ * <p>
+ * push(x) -- Push element x onto stack.
+ * pop() -- Removes the element on top of the stack.
+ * top() -- Get the top element.
+ * empty() -- Return whether the stack is empty.
+ * Example:
+ * <p>
+ * MyStack stack = new MyStack();
+ * <p>
+ * stack.push(1);
+ * stack.push(2);
+ * stack.top();   // returns 2
+ * stack.pop();   // returns 2
+ * stack.empty(); // returns false
+ * Notes:
+ * <p>
+ * You must use only standard operations of a queue -- which means only push to back, peek/pop from front, size, and is empty operations are valid.
+ * Depending on your language, queue may not be supported natively. You may simulate a queue by using a list or deque (double-ended queue), as long as you use only standard operations of a queue.
+ * You may assume that all operations are valid (for example, no pop or top operations will be called on an empty stack).
+ * <p>
+ * 20190222
+ */
+public class ImplementStackUsingQueues {
+    Deque<Integer> deque = new LinkedList<>();
+
+    /**
+     * Initialize your data structure here.
+     */
+//    public MyStack() {
+//
+//    }
+
+    /**
+     * Push element x onto stack.
+     */
+    public void push(int x) {
+        deque.offer(x);
+    }
+
+    /**
+     * Removes the element on top of the stack and returns that element.
+     */
+    public int pop() {
+        if (!deque.isEmpty()) {
+            return deque.removeLast();
+        }
+        return -1;
+    }
+
+    /**
+     * Get the top element.
+     */
+    public int top() {
+        if (!deque.isEmpty()) {
+            return deque.getLast();
+        }
+        return -1;
+    }
+
+    /**
+     * Returns whether the stack is empty.
+     */
+    public boolean empty() {
+        return deque.isEmpty();
+    }
+}
