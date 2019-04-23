8aebed741f786c5eaea2f5c6762792f778fc4182
chaangliu <chaangliu@tencent.com>
2019-04-23 23:38:27 +0800

    576. Out of Boundary Paths --dfs with memo

--- dfs/OutOfBoundaryPaths.java
+++ dfs/OutOfBoundaryPaths.java
@@ -1,5 +1,9 @@
 package dfs;
 
+import android.widget.HeterogeneousExpandableList;
+
+import java.util.Arrays;
+
 /**
  * There is an m by n grid with a ball. Given the start coordinate (i,j) of the ball, you can move the ball to adjacent cell or cross the grid boundary in four directions (up, down, left, right). However, you can at most move N times. Find out the number of paths to move the ball out of grid boundary. The answer may be very large, return it after mod 109 + 7.
  * Example 1:
@@ -52,5 +56,37 @@ public class OutOfBoundaryPaths {
                 + findPath__TTLE(m, n, N - 1, i + 1, j)
                 + findPath__TTLE(m, n, N - 1, i, j - 1)
                 + findPath__TTLE(m, n, N - 1, i, j + 1);
+    }
+
+    /**
+     * approach1 with cache
+     * 对于每个格子，记录从那个格子开始走有多少种答案，以后来到那个路口就不需要再走下去了，跟DP一样
+     * 这里用了三维数组，第三维表示路径。
+     * <p>
+     * 这里真正写起来的时候我发现memo[i][j][N] = helper + .... 以及最后一行return memo[i][j][N]很巧妙。另外这种写法的mod很折磨人，必须前两个mod后两个mod最后再mod不然WA。
+     */
+
+    int mod = (int) (1e9 + 7);
+
+    public int findPaths(int m, int n, int N, int i, int j) {
+        //第三个参数长度是N + 1,因为对于每个格子来说，剩余的步数不同，结果也不一样
+        int[][][] memo = new int[m][n][N + 1];
+        for (int[][] grid : memo)
+            for (int[] row : grid)
+                Arrays.fill(row, -1);
+        return helper(m, n, N, i, j, memo);
+    }
+
+    private int helper(int m, int n, int N, int i, int j, int[][][] memo) {
+        if (i < 0 || i >= m || j < 0 || j >= n) return 1;
+        if (memo[i][j][N] != -1) return memo[i][j][N];
+        if (N == 0) return 0;
+        memo[i][j][N] = ((helper(m, n, N - 1, i - 1, j, memo) + helper(m, n, N - 1, i + 1, j, memo)) % mod
+                + (helper(m, n, N - 1, i, j - 1, memo) + helper(m, n, N - 1, i, j + 1, memo)) % mod) % mod;
+        return memo[i][j][N];
+    }
+
+    public static void main(String args[]) {
+        System.out.println(new OutOfBoundaryPaths().findPaths(2, 2, 2, 0, 0));
     }
 }
