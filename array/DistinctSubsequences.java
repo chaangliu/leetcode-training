a801982fa5d1a9062c4e9a58fa8d251777e1e61c
chaangliu <chaangliu@tencent.com>
2019-05-03 21:40:57 +0800

    115. Distinct Subsequences --review

--- array/DistinctSubsequences.java
+++ array/DistinctSubsequences.java
@@ -22,33 +22,34 @@ package array;
  */
 
 public class DistinctSubsequences {
-    int maxLen = 0;
-    int count = 0;
 
+    /**
+     * 20190503review
+     * 我感觉这题可以看做求LCS的个数，但是真正做的时候发现套用LCS的转移方程是行不通的，因为无法统计LCS正好匹配到之前有多少种重复情况满足。
+     */
     public int numDistinct(String s, String t) {
         if (s.length() == 0 || t.length() == 0) return 0;
         int dp[][] = new int[s.length() + 1][t.length() + 1];
+        //如果t.length() == 0，那么对于s的前i位结果都是1
         for (int i = 0; i < s.length() + 1; i++) {
             dp[i][0] = 1;
-
         }
         for (int i = 1; i < s.length() + 1; i++)
             for (int j = 1; j < t.length() + 1; j++) {
                 if (s.charAt(i - 1) != t.charAt(j - 1)) {
                     dp[i][j] = dp[i - 1][j];
                 } else {
+                    //dp[i - 1][j - 1]的意思是，既然s[i]和t[j]相等，那么在同时加上这两个数之前的结果都可以算进去
+                    //dp[i - 1][j]的意思是，不考虑加上s[i]，但是加入t[j]，满足条件的个数。
+                    //至于为什么不计算dp[i][j - 1]，因为是求s中有多少个子序列可以组成t，t是固定的。
                     dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
                 }
             }
         return dp[s.length()][t.length()];
     }
 
-
-
     public static void main(String args[]) {
         DistinctSubsequences distinctSubsequences = new DistinctSubsequences();
         System.out.println(distinctSubsequences.numDistinct("rabbbit", "rabbit"));
-//        for(int i = 0 ; i < 7 ; i++)
-//            System.out.println();
     }
 }
