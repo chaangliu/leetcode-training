e28433fa2e9ef051ef59c55c01eb89832d7e3e5d
chaangliu <chaangliu@tencent.com>
2019-07-05 11:24:50 +0800

    1105. Filling Bookcase Shelves --review 2

--- dp/FillingBookcaseShelves.java
+++ dp/FillingBookcaseShelves.java
@@ -48,14 +48,14 @@ public class FillingBookcaseShelves {
         int dp[] = new int[books.length + 1];
         dp[0] = 0;
         for (int i = 0; i < books.length; i++) {
-            int width = books[i][0];//上一层的累计宽度
+            int width = books[i][0];//错误1 会造成重复plus
             int height = books[i][1];
-            dp[i + 1] = dp[i] + height;//放在新的一层
-            for (int j = i; j > 0; j--) {
+            dp[i + 1] = dp[i] + height;
+            for (int j = i; j > 0; j--) {//错误2 漏掉j = 0
                 width += books[j][0];
                 if (width > shelf_width) break;
                 height = Math.max(height, books[j][1]);
-                dp[i + 1] = Math.min(dp[i + 1], dp[j - 1] + height);
+                dp[i + 1] = Math.min(dp[i + 1], dp[j - 1] + height);//错误3 dp[j]其实就相当于books[0, j - 1]的最优秀情况，无需-1
             }
         }
         return dp[dp.length - 1];
@@ -63,8 +63,9 @@ public class FillingBookcaseShelves {
 
     /**
      * B. 下标从0开始的正确代码：
+     * 这种会比下标从1开始的方案循环次数多
      */
-    public int minHeightShelves___(int[][] books, int shelf_width) {
+    public int minHeightShelves_(int[][] books, int shelf_width) {
         int dp[] = new int[books.length + 1];
         dp[0] = 0;
         for (int i = 0; i < books.length; i++) {
@@ -74,6 +75,7 @@ public class FillingBookcaseShelves {
             for (int j = i; j >= 0; j--) {
                 width += books[j][0];
                 if (width > shelf_width) break;
+                System.out.println("当前最高：" + height + ", 新书高度：" + books[j][1]);
                 height = Math.max(height, books[j][1]);//books[j -1]到books[i]中的最高一本书的高度
                 dp[i + 1] = Math.min(dp[i + 1], dp[j] + height);//dp[j]对应的是books[0]到books[j - 1]这本书为止的最小书架高度
             }
@@ -83,6 +85,7 @@ public class FillingBookcaseShelves {
 
     /**
      * C. 下标从1开始的正确代码（推荐，更容易理解，大部分人用了这种）：
+     * 书本下标比dp下标小1，我们假设以后遇到这种题都这么处理
      */
     public int minHeightShelves__(int[][] books, int shelf_width) {
         int[] dp = new int[books.length + 1];
@@ -91,7 +94,8 @@ public class FillingBookcaseShelves {
             int width = books[i - 1][0];
             int height = books[i - 1][1];
             dp[i] = dp[i - 1] + height;
-            for (int j = i - 1; j > 0 && width + books[j - 1][0] <= shelf_width; --j) {
+            for (int j = i - 1; j > 0 && width + books[j - 1][0] <= shelf_width; --j) {//j > 0, 所以i = 1的时候不会执行这一层。
+                System.out.println("--当前最高：" + height + ", 新书高度：" + books[j - 1][1]);
                 height = Math.max(height, books[j - 1][1]);//books[j -1]到books[i]中的最高一本书的高度
                 width += books[j - 1][0];
                 dp[i] = Math.min(dp[i], dp[j - 1] + height);//dp[j - 1]对应的是books[0]到books[j - 2]这本书为止的最小书架高度（重点）
@@ -102,7 +106,7 @@ public class FillingBookcaseShelves {
 
     public static void main(String args[]) {
         int[][] arr = new int[][]{{7, 3}, {8, 7}, {2, 7}, {2, 5}};
-        new Test().minHeightShelves(arr, 10);
-        new Test().minHeightShelves__(arr, 10);
+        new FillingBookcaseShelves().minHeightShelves_(arr, 10);
+        new FillingBookcaseShelves().minHeightShelves__(arr, 10);
     }
 }
