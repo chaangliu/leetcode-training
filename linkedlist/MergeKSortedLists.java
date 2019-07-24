c986a70fb541abf0ab984751ba1f2144f99665ae
chaangliu <chaangliu@tencent.com>
2019-07-24 23:06:25 +0800

    373. Find K Pairs with Smallest Sums

--- linkedlist/MergeKSortedLists.java
+++ linkedlist/MergeKSortedLists.java
@@ -7,6 +7,7 @@ import linkedlist.ListNode;
 
 /**
  * Created by DrunkPiano on 2017/3/27.
+ * 相似题目：Find K Pairs With Smallest Sums
  */
 
 public class MergeKSortedLists {
@@ -19,18 +20,15 @@ public class MergeKSortedLists {
                 return o1.val - o2.val;
             }
         });
-
         for (ListNode list : lists) {
             if (list != null)
                 queue.offer(list);
         }
-
         ListNode dummy = new ListNode(-1);
         ListNode tail = dummy;
         while (!queue.isEmpty()) {
             tail.next = queue.poll();
             tail = tail.next;
-
             //把每个链表上的node全撸下来加入到queue,注意这个撸下来的过程可能不是连续的，可能先撸了第一个链表的next就去撸下一个链表了
             if (tail.next != null) {
                 queue.offer(tail.next);
@@ -40,22 +38,22 @@ public class MergeKSortedLists {
     }
 
 
-	public static void main(String[] args) {
-		PriorityQueue<String> pq = new PriorityQueue<String>();
-		pq.add("dog");
-		pq.add("apple");
-		pq.add("fox");
-		pq.add("easy");
-		pq.add("boy");
+    public static void main(String[] args) {
+        PriorityQueue<String> pq = new PriorityQueue<String>();
+        pq.add("dog");
+        pq.add("apple");
+        pq.add("fox");
+        pq.add("easy");
+        pq.add("boy");
 
-		while (!pq.isEmpty()) {
-			System.out.print("left:");
-			for (String s : pq) {
-				System.out.print(s + " ");
-			}
-			System.out.println();
-			System.out.println("poll(): " + pq.poll());
-		}
-	}
+        while (!pq.isEmpty()) {
+            System.out.print("left:");
+            for (String s : pq) {
+                System.out.print(s + " ");
+            }
+            System.out.println();
+            System.out.println("poll(): " + pq.poll());
+        }
+    }
 
 }
