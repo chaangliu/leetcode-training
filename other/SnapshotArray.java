351d95f54012895e5792787f4f788d59b6f92244
chaangliu <chaangliu@tencent.com>
2019-08-04 18:42:15 +0800

    1144. Decrease Elements To Make Array Zigzag

---
+++ other/SnapshotArray.java
@@ -0,0 +1,81 @@
+package other;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * Implement a SnapshotArray that supports the following interface:
+ * <p>
+ * SnapshotArray(int length) initializes an array-like data structure with the given length.  Initially, each element equals 0.
+ * void set(index, val) sets the element at the given index to be equal to val.
+ * int snap() takes a snapshot of the array and returns the snap_id: the total number of times we called snap() minus 1.
+ * int get(index, snap_id) returns the value at the given index, at the time we took the snapshot with the given snap_id
+ * <p>
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: ["SnapshotArray","set","snap","set","get"]
+ * [[3],[0,5],[],[0,6],[0,0]]
+ * Output: [null,null,0,null,5]
+ * Explanation:
+ * SnapshotArray snapshotArr = new SnapshotArray(3); // set the length to be 3
+ * snapshotArr.set(0,5);  // Set array[0] = 5
+ * snapshotArr.snap();  // Take a snapshot, return snap_id = 0
+ * snapshotArr.set(0,6);
+ * snapshotArr.get(0,0);  // Get the value of array[0] with snap_id = 0, return 5
+ * <p>
+ * <p>
+ * Constraints:
+ * <p>
+ * 1 <= length <= 50000
+ * At most 50000 calls will be made to set, snap, and get.
+ * 0 <= index < length
+ * 0 <= snap_id < (the total number of times we call snap())
+ * 0 <= val <= 10^9
+ * 20190804
+ */
+public class SnapshotArray {
+    Map<Integer, Integer> item = new HashMap<>();
+    Map<Integer, Map<Integer, Integer>> snaps = new HashMap<>();
+    int snap_id = 0;
+
+    public SnapshotArray(int length) {
+
+    }
+
+    public void set(int index, int val) {
+        item.put(index, val);
+    }
+
+    public int snap() {
+        snaps.put(snap_id++, item);
+        item = new HashMap<>();
+        Set<Map.Entry<Integer, Integer>> set = snaps.get(snap_id - 1).entrySet();
+        for (Map.Entry<Integer, Integer> entry : set) {
+            item.put(entry.getKey(), entry.getValue());
+        }
+        return snap_id - 1;
+    }
+
+    public int get(int index, int snap_id) {
+        if (!snaps.containsKey(snap_id) || !snaps.get(snap_id).containsKey(index)) return 0;
+        return snaps.get(snap_id).get(index);
+    }
+
+    /**
+     * ["SnapshotArray","snap","snap","get","set","snap","set"]
+     * [[4],[],[],[3,1],[2,4],[],[1,4]]
+     * <p>
+     * ["SnapshotArray","set","snap","snap","snap","get","snap","snap","get"]
+     * [[1],[0,15],[],[],[],[0,2],[],[],[0,0]]
+     */
+    public static void main(String args[]) {
+        SnapshotArray obj = new SnapshotArray(1);
+        obj.set(0, 15);
+        obj.snap();
+        obj.snap();
+        obj.snap();
+        obj.get(0, 2);
+    }
+}
