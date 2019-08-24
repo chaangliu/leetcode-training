15c2a333971643352231f0904fc15eb872976e72
chaangliu <chaangliu@tencent.com>
2019-08-24 14:55:50 +0800

    452. Minimum Number of Arrows to Burst Balloons

---
+++ greedy/MinimumNumberofArrowstoBurstBalloons.java
@@ -0,0 +1,44 @@
+package greedy;
+
+import java.util.Arrays;
+
+/**
+ * There are a number of spherical balloons spread in two-dimensional space. For each balloon, provided input is the start and end coordinates of the horizontal diameter. Since it's horizontal, y-coordinates don't matter and hence the x-coordinates of start and end of the diameter suffice. Start is always smaller than end. There will be at most 104 balloons.
+ * <p>
+ * An arrow can be shot up exactly vertically from different points along the x-axis. A balloon with xstart and xend bursts by an arrow shot at x if xstart ≤ x ≤ xend. There is no limit to the number of arrows that can be shot. An arrow once shot keeps travelling up infinitely. The problem is to find the minimum number of arrows that must be shot to burst all balloons.
+ * <p>
+ * Example:
+ * <p>
+ * Input:
+ * [[10,16], [2,8], [1,6], [7,12]]
+ * <p>
+ * Output:
+ * 2
+ * <p>
+ * Explanation:
+ * One way is to shoot one arrow for example at x = 6 (bursting the balloons [2,8] and [1,6]) and another arrow at x = 11 (bursting the other two balloons).
+ * 20190824
+ * <p>
+ * 56 Merge Intervals <- very similar😈
+ * 435 Non-overlapping Intervals <- very similar😈
+ * 252 Meeting Rooms
+ * 253 Meeting Rooms II
+ */
+public class MinimumNumberofArrowstoBurstBalloons {
+    /**
+     * 和435一样，interval schedule问题。贪心地计算最多不重合的interval，结果就是最小弓箭数量
+     **/
+    public int findMinArrowShots(int[][] points) {
+        if (points.length == 0) return 0;
+        Arrays.sort(points, (a, b) -> (a[1] - b[1]));
+        int end = points[0][1];
+        int cnt = 1;
+        for (int i = 1; i < points.length; i++) {
+            if (points[i][0] > end) {
+                end = points[i][1];
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+}
