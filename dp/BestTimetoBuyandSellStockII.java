acd8dfdee0edb3707368e3f5011faf3f0746f3d6
chaangliu <chaangliu@tencent.com>
2019-08-31 23:37:55 +0800

    714. Best Time to Buy and Sell Stock with Transaction Fee

--- dp/BestTimetoBuyandSellStockII.java
+++ dp/BestTimetoBuyandSellStockII.java
@@ -20,15 +20,53 @@ package dp;
 
 public class BestTimetoBuyandSellStockII {
 
-//    public int maxProfit(int[] prices) {
-//        if (prices.length < 2) return 0;
-//        for (int i = 1 ; i < prices.length ; i ++){
-//
-//
-//        }
-//
-//    }
+    /**
+     * 官方题解III，也是solutions的高票解法。用[1, 7, 2, 3, 6, 7, 6, 7]可以看出道理。
+     * 比如2-3的那天，3的那天我卖掉，就可以获利1，可以理解为3那天我卖掉了但是同时又买回来了（因为根据上帝视角知道后一天是6）。
+     */
+    public int maxProfit(int[] prices) {
+        int maxprofit = 0;
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] > prices[i - 1])
+                maxprofit += prices[i] - prices[i - 1];
+        }
+        return maxprofit;
+    }
 
+    /**
+     * 以下是解决股票问题模板那个回答里面提到的通用做法(https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock-with-cooldown/solution/yi-ge-fang-fa-tuan-mie-6-dao-gu-piao-wen-ti-by-lab/)；
+     * 0代表当天结束不持有股票，1代表持有股票，转移方程：
+     * dp[i][0] = max(dp[i - 1][0], dp[i - 1][1] + prices[i])
+     * dp[i][1] = max(dp[i - 1][1], dp[i - 1][0] - prices[i])
+     */
+
+    public int maxProfit__(int[] prices) {
+        int n = prices.length;
+        if (n == 0) return 0;
+        int[][] dp = new int[n][2];
+        dp[0][0] = 0;
+        dp[0][1] = -prices[0];
+        for (int i = 1; i < n; i++) {
+            dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i]);
+            dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] - prices[i]);
+        }
+        return dp[n - 1][0];
+    }
+
+    /**
+     * 上面的2D由于i只依赖i-1状态，可以进而转1D滚动数组:
+     */
+    public int maxProfit_(int[] prices) {
+        int T_ik0 = 0, T_ik1 = Integer.MIN_VALUE;
+
+        for (int price : prices) {
+            int T_ik0_old = T_ik0;
+            T_ik0 = Math.max(T_ik0, T_ik1 + price);
+            T_ik1 = Math.max(T_ik1, T_ik0_old - price);
+        }
+
+        return T_ik0;
+    }
 
     public static void main(String args[]) {
         BestTimetoBuyandSellStockII instance = new BestTimetoBuyandSellStockII();
---
+++ dp/BestTimetoBuyandSellStockwithTransactionFee.java
@@ -0,0 +1,85 @@
+package dp;
+
+/**
+ * Your are given an array of integers prices, for which the i-th element is the price of a given stock on day i; and a non-negative integer fee representing a transaction fee.
+ * <p>
+ * You may complete as many transactions as you like, but you need to pay the transaction fee for each transaction. You may not buy more than 1 share of a stock at a time (ie. you must sell the stock share before you buy again.)
+ * <p>
+ * Return the maximum profit you can make.
+ * <p>
+ * Example 1:
+ * Input: prices = [1, 3, 2, 8, 4, 9], fee = 2
+ * Output: 8
+ * Explanation: The maximum profit can be achieved by:
+ * Buying at prices[0] = 1
+ * Selling at prices[3] = 8
+ * Buying at prices[4] = 4
+ * Selling at prices[5] = 9
+ * The total profit is ((8 - 1) - 2) + ((9 - 4) - 2) = 8.
+ * Note:
+ * <p>
+ * 0 < prices.length <= 50000.
+ * 0 < prices[i] < 50000.
+ * 0 <= fee < 50000.
+ * 20190831
+ */
+public class BestTimetoBuyandSellStockwithTransactionFee {
+    /**
+     * 参考股票问题的通用解法写出来的(https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock-with-cooldown/solution/yi-ge-fang-fa-tuan-mie-6-dao-gu-piao-wen-ti-by-lab/)
+     * 那个模板真不错，我感觉没有模板很难归纳出来，遇到新的就歇逼，或者当天看懂了过几天就歇逼
+     * 这题代码实现上和BestTimetoBuyandSellStockII的唯一区别就是需要在卖出去的时候减去一个fee。
+     * 0代表当天结束不持有股票，1代表持有股票，转移方程：
+     * dp[i][0] = max(dp[i - 1][0], dp[i - 1][1] + prices[i] - fee)
+     * dp[i][1] = max(dp[i - 1][1], dp[i - 1][0] - prices[i])
+     * 上面链接里的通用转移方程确实很不错，但是他对于可以进行无限次交易的时候k为什么可以省略解释得很confusing，
+     * 说因为infinity的情况下k == k - 1；我觉得这么理解，因为与k无关，所以无需k这个维度，当然这也是naive的思维，真正的证明可能还是跟折线图有关
+     */
+    public int maxProfit(int[] prices, int fee) {
+        int n = prices.length;
+        int[][] dp = new int[n][2];
+        dp[0][0] = 0;
+        dp[0][1] = -prices[0];
+        for (int i = 1; i < n; i++) {
+            dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i] - fee);
+            dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] - prices[i]);
+        }
+        return dp[n - 1][0];
+    }
+
+    /**
+     * ==>滚动数组
+     */
+    public int maxProfit_(int[] prices, int fee) {
+        int n = prices.length;
+        if (n == 0) return 0;
+        int notHold = 0, hold = -prices[0];
+        for (int i = 1; i < n; i++) {
+            int tmp = notHold;
+            notHold = Math.max(tmp, hold + prices[i] - fee);
+            hold = Math.max(hold, tmp - prices[i]);
+        }
+        return notHold;
+    }
+
+    /**
+     【附上模板】
+     * dp[i][k][0] = max(dp[i-1][k][0], dp[i-1][k][1] + prices[i])
+     max(   选择 rest  ,           选择 sell      )
+
+     解释：今天我没有持有股票，有两种可能：
+     要么是我昨天就没有持有，然后今天选择 rest，所以我今天还是没有持有；
+     要么是我昨天持有股票，但是今天我 sell 了，所以我今天没有持有股票了。
+
+     dp[i][k][1] = max(dp[i-1][k][1], dp[i-1][k-1][0] - prices[i])
+     max(   选择 rest  ,           选择 buy         )
+
+     解释：今天我持有着股票，有两种可能：
+     要么我昨天就持有着股票，然后今天选择 rest，所以我今天还持有着股票；
+     要么我昨天本没有持有，但今天我选择 buy，所以今天我就持有股票了。
+
+     作者：labuladong
+     链接：https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock-with-cooldown/solution/yi-ge-fang-fa-tuan-mie-6-dao-gu-piao-wen-ti-by-lab/
+     来源：力扣（LeetCode）
+     著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+}
