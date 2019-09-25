3e46832c9e4f8c5cda06b98bb7b99f59306c44ad
chaangliu <chaangliu@tencent.com>
2019-09-25 17:31:50 +0800

    110. Balanced Binary Tree --review

--- tree/BalancedBinaryTree.java
+++ tree/BalancedBinaryTree.java
@@ -1,30 +1,62 @@
 package tree;
 
 /**
+ * Given a binary tree, determine if it is height-balanced.
+ *
+ * For this problem, a height-balanced binary tree is defined as:
+ *
+ * a binary tree in which the depth of the two subtrees of every node never differ by more than 1.
+ *
+ * Example 1:
+ *
+ * Given the following tree [3,9,20,null,null,15,7]:
+ *
+ *     3
+ *    / \
+ *   9  20
+ *     /  \
+ *    15   7
+ * Return true.
+ *
+ * Example 2:
+ *
+ * Given the following tree [1,2,2,3,3,null,null,4,4]:
+ *
+ *        1
+ *       / \
+ *      2   2
+ *     / \
+ *    3   3
+ *   / \
+ *  4   4
+ * Return false.
  * Created by DrunkPiano on 2017/4/2.
  */
 
 public class BalancedBinaryTree {
-    //    public boolean isBalanced(TreeNode root) {
-//        return helper(root) >= 0;
-//    }
-//
-//    private int helper(TreeNode root) {
-//        if (root == null) return 0;
-//        //这样会把所有node都当做root遍历一遍。TimeComplexity:O(n)
-//        int lh = helper(root.left);
-//        int rh = helper(root.right);
-//
-//        if (lh == -1 || rh == -1) {
-//            return -1;
-//        }
-//        if (Math.abs(lh - rh) >= 2) {
-//            //技巧：返回-1代表某棵subtree imbalance 了
-//            return -1;
-//        }
-//        return Math.max(helper(root.left), helper(root.right)) + 1;
-//    }
+    /**
+     * bottom up, TimeComplexity:O(n)
+     *
+     * 这题是剑指offer原题。bottom up效率高， top down有很多重复子问题。
+     */
     public boolean isBalanced(TreeNode root) {
+        return depth(root) != -1;
+    }
+
+    private int depth(TreeNode root) {
+        if (root == null) return 0;
+        int lh = depth(root.left);
+        if(lh == -1) return  -1;
+        int rh = depth(root.right);
+        if (rh == -1) return -1;
+        return Math.abs(lh - rh) > 1 ? -1 : Math.max(lh, rh) + 1;
+    }
+
+
+    /**
+     * top down, slow
+     */
+    public boolean isBalanced_SLOW(TreeNode root) {
         if (root == null) return true;
         int lh = height(root.left);
         int rh = height(root.right);
