6f1d01342c7de93e5e02cba7f99fbe9332219b3f
chaangliu <chaangliu@tencent.com>
2019-10-01 08:57:15 +0800

    128. Longest Consecutive Sequence

---
+++ hashtable/LongestConsecutiveSequence.java
@@ -0,0 +1,49 @@
+package hashtable;
+
+import java.util.HashSet;
+
+/**
+ * Given an unsorted array of integers, find the length of the longest consecutive elements sequence.
+ * <p>
+ * Your algorithm should run in O(n) complexity.
+ * <p>
+ * Example:
+ * <p>
+ * Input: [100, 4, 200, 1, 3, 2]
+ * Output: 4
+ * Explanation: The longest consecutive elements sequence is [1, 2, 3, 4]. Therefore its length is 4.
+ * 20191001
+ */
+public class LongestConsecutiveSequence {
+    /**
+     * 这题是说在无需数列里找连续数列的最大长度。要求O(n)，我一开始想quick select？仔细想想不行。
+     * TAG标了一个UnionFind但是大部分人用了Hash。
+     * 我看了一下HashSet的提示，写出了下面的代码，早上急匆匆的WA了好几次。
+     */
+    public int longestConsecutive(int[] nums) {
+        if (nums.length == 0) return 0;
+        HashSet<Integer> all = new HashSet<>();
+        for (int num : nums) all.add(num);
+        HashSet<Integer> visited = new HashSet<>();
+        int len = all.size();
+        int maxStreak = 1, curStreak = 1;
+        for (int i = 0; i < len; i++) {
+            if (visited.contains(nums[i])) continue;
+            int l = nums[i] - 1, r = nums[i] + 1;
+            while (all.contains(l) && !visited.contains(l)) {
+                curStreak++;
+                visited.add(l);
+                l--;
+            }
+            while (all.contains(r) && !visited.contains(r)) {
+                curStreak++;
+                visited.add(r);
+                r++;
+            }
+            maxStreak = Math.max(maxStreak, curStreak);
+            curStreak = 1;
+
+        }
+        return maxStreak;
+    }
+}
