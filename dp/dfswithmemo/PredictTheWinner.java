68129d5e5a1257b4731dbd95ee5b928e607f1dd9
chaangliu <chaangliu@tencent.com>
2019-10-29 11:19:29 +0800

    486. Predict the Winner --review

--- dp/dfswithmemo/PredictTheWinner.java
+++ dp/dfswithmemo/PredictTheWinner.java
@@ -21,22 +21,25 @@ package dp.dfswithmemo;
  * 1 <= length of the array <= 20.
  * Any scores in the given array are non-negative integers and will not exceed 10,000,000.
  * If the scores of both players are equal, then player 1 is still the winner.
+ * TAG:[MINMAX][DP]
  * 20190920
  */
 public class PredictTheWinner {
     /**
-     * 这题挺抽象的，转移方程我看了好久，最后看例子才看懂。
+     * 题意：一个长度最多20的数组里存放正整数，我和对手每次从两端分别取一个数字，每个人都全局最优地去取，问取完后我能拿到的分数>=对方的分数的概率。
+     * 这题也是top down的dfs with memo容易理解；bottom up挺抽象的，转移方程我看了好久，最后看例子才看懂。
      * Approach 1, dfs with memo
-     * dp[i][j]代表[i,j]范围先手的人比后手的人多拿的score。比如[1,5,2]，我选1，然后对方成了先手，在5,2里选一个，他一定可以多拿3。也就是说我当前拿的必须比他即将多拿的还要多才能赢
+     * dp[i][j]（dfs里也就是helper函数）代表[i,j]范围先手的人比后手的人多拿的score。比如[1,5,2]，我选1，然后对方成了先手，在5,2里选一个，他一定可以多拿3。也就是说我当前拿的必须比他即将多拿的还要多才能赢
      */
-
     public boolean PredictTheWinner(int[] nums) {
         return helper(0, nums.length - 1, nums) >= 0;
     }
 
     //helper返回palyer1 - palyer2的score差值
+    //注意，题目说you can assume each player plays to maximize his score，递归的时候需要模拟我选左边或者选右边然后取一个最大值，而不是简单地贪心地选两者中较大的一个。
+    //例如，[4,3,100,9]，如果我选9对方就能选到100，所以我要优先选择4。
     private int helper(int l, int r, int[] nums) {
-        return l == r ? nums[l] : Math.max(nums[l] - helper(l + 1, r, nums), nums[r] - helper(l, r - 1, nums));
+        return l == r ? nums[l] : Math.max(nums[l] - helper(l + 1, r, nums), nums[r] - helper(l, r - 1, nums));//我当前多拿的 - 他即将多拿的
     }
 
     /**
@@ -55,6 +58,25 @@ public class PredictTheWinner {
         return memo[l][r];
     }
 
+    /**
+     * 相似题目：
+     * 19年某次G家校招一面：
+     * 有一个数组A，你每次可以从左或者从右取一个数，现在要你取k次，每次价值是M[i]*取的数 求最大价值
+     * 试着写一下：
+     */
+    public int maxProfit(int[] A, int[] M, int K) {
+        return dfs(0, A.length - 1, A, M, new Integer[A.length - 1][A.length - 1], K);
+    }
+
+    //dfs返回[l,r]内取K次的最大profit
+    private int dfs(int l, int r, int[] A, int[] M, Integer[][] memo, int K) {
+        if (K == 0) return 0;
+        if (l == r) return A[l] * M[l];
+        int ll = memo[l + 1][r] != null ? memo[l + 1][r] : dfs(l + 1, r, A, M, memo, K - 1);
+        int rr = memo[l][r - 1] != null ? memo[l][r - 1] : dfs(l, r - 1, A, M, memo, K - 1);
+        memo[l][r] = Math.max(A[l] * M[l] + ll, A[r] * M[r] + rr);
+        return memo[l][r];
+    }
 
     /**
      * Approach 2, 改写成dp(还能转1D的)
