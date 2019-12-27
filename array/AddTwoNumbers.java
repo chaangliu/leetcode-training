6e7c6705c2d1b940f0bf13f6f196b828bc7dc3c7
chaangliu <chaangliu@tencent.com>
2019-12-27 13:46:44 +0800

    2. Add Two Numbers

--- array/AddTwoNumbers.java
+++ array/AddTwoNumbers.java
@@ -1,31 +1,47 @@
 package array;
 
 
+import linkedlist.ListNode;
+
 /**
+ * You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
+ * You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+ * Example:
  * Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
  * Output: 7 -> 0 -> 8
- * <p>
- * 这题意思是，数字是反过来存储在linked list里的，比如上面那个其实就是计算
- * 342
- * 465 = 807的过程。
+ * Explanation: 342 + 465 = 807.
  */
 
 public class AddTwoNumbers {
-
-    static class ListNode {
-        ListNode next;
-        int val;
-
-        public ListNode(int x) {
-            val = x;
+    /**
+     * 题意：两个链表代表两个数字，每一位反向存储在链表里，求相加的结果，输出仍用链表表示。
+     * 解法：容易错的地方是，在两个list都end之后还要考虑有没有额外的进位
+     * 20191227 review
+     **/
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(-1), cur = dummy;
+        int carry = 0;
+        while (l1 != null || l2 != null) {
+            int v1 = l1 != null ? l1.val : 0;
+            int v2 = l2 != null ? l2.val : 0;
+            int sum = v1 + v2 + carry;
+            int num = sum % 10;
+            carry = sum / 10;
+            cur.next = new ListNode(num);
+            cur = cur.next;
+            if (l1 != null) l1 = l1.next;//已犯错误1. 忘记迭代l1 l2
+            if (l2 != null) l2 = l2.next;
         }
+        if (carry == 1) cur.next = new ListNode(1);//已犯错误2. 忘记考虑有没有额外的进位
+        return dummy.next;
     }
 
-    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-
+    /**
+     * 老代码
+     */
+    public ListNode addTwoNumbers__(ListNode l1, ListNode l2) {
         int carry = 0;
         ListNode p1 = l1, p2 = l2, fakeHead = new ListNode(-1), p3 = fakeHead;
-
         while (p1 != null || p2 != null) {
             if (p1 != null) {
                 carry += p1.val;
@@ -35,7 +51,6 @@ public class AddTwoNumbers {
                 carry += p2.val;
                 p2 = p2.next;
             }
-
             p3.next = new ListNode(carry % 10);
             p3 = p3.next;
             carry = carry / 10;
@@ -43,24 +58,5 @@ public class AddTwoNumbers {
         if (carry == 1)
             p3.next = new ListNode(carry);
         return fakeHead.next;
-    }
-
-    public static void main(String args[]) {
-        AddTwoNumbers addTwoNumbers = new AddTwoNumbers();
-
-        ListNode l1 = new ListNode(2);
-        ListNode l1Next1 = new ListNode(4);
-        ListNode l1Next2 = new ListNode(6);
-        l1.next = l1Next1;
-        l1Next1.next = l1Next2;
-
-        ListNode l2 = new ListNode(8);
-        ListNode l2Next1 = new ListNode(6);
-        ListNode l2Next2 = new ListNode(4);
-        l2.next = l2Next1;
-        l2Next1.next = l2Next2;
-
-        System.out.println(addTwoNumbers.addTwoNumbers(l1, l2));
-        System.out.println("finish");
     }
 }
