24436e08d9f280974c7413522c4e86862b151060
liuchang <lcconan@gmail.com>
2020-01-01 22:07:53 +0800

    17. Letter Combinations of a Phone Number --review

--- array/LongestCommonPrefix.java
+++ array/LongestCommonPrefix.java
@@ -1,63 +1,38 @@
 package array;
 
 /**
+ * Write a function to find the longest common prefix string amongst an array of strings.
+ * If there is no common prefix, return an empty string "".
+ * Example 1:
+ * Input: ["flower","flow","flight"]
+ * Output: "fl"
+ * Example 2:
+ * Input: ["dog","racecar","car"]
+ * Output: ""
+ * Explanation: There is no common prefix among the input strings.
+ * Note:
+ * All given inputs are in lowercase letters a-z.
  * Created by DrunkPiano on 17/05/2017.
+ * 20200101
  */
 
 public class LongestCommonPrefix {
-//	public String longestCommonPrefix(String[] strs) {
-//		if (strs == null || strs.length == 0) return "";
-//		if (strs.length == 1) return strs[0];
-//		int minLen = Integer.MAX_VALUE;
-//		for (String s : strs) {
-//			minLen = Math.min(s.length(), minLen);
-//		}
-//		if (minLen == 0) return "";
-//		int end = 1;
-//		for (; end <= minLen; end++) {
-//			for (String s : strs) {
-//				String temp = strs[0].substring(0, end);
-//				if (!s.substring(0, end).equals(temp))
-//					return strs[0].substring(0, end - 1);
-//			}
-//		}
-//		int  s = strs[0].indexOf("a");
-//
-//		return strs[0].substring(0, end-1);
-//
-//	}
-
-	//	divide and conquer
-	public String longestCommonPrefix(String[] strs) {
-		if (strs == null || strs.length == 0) return "";
-		return longestCommonPrefix(strs, 0, strs.length - 1);
-	}
-
-	private String longestCommonPrefix(String[] strs, int l, int r) {
-		if (l == r) {
-			return strs[l];
-		} else {
-			int mid = (l + r) / 2;
-			String lcpLeft = longestCommonPrefix(strs, l, mid);
-			String lcpRight = longestCommonPrefix(strs, mid + 1, r);
-			return commonPrefix(lcpLeft, lcpRight);
-		}
-	}
-
-	String commonPrefix(String left, String right) {
-		int min = Math.min(left.length(), right.length());
-		for (int i = 0; i < min; i++) {
-			if (left.charAt(i) != right.charAt(i))
-				return left.substring(0, i);
-		}
-		return left.substring(0, min);
-	}
-
-	public static void main(String args[]) {
-//		String[] s = {"acff", "aca", "accb", "ac"};
-//		System.out.println(new LongestCommonPrefix().longestCommonPrefix(s));
-		String s = "轻质防滑粉";
-		System.out.println(s.length());
-		System.out.println(s.substring(0,2));
-	}
+    /**
+     * 题意：求最长公共前缀
+     * 解法：linear scan
+     */
+    public String longestCommonPrefix(String[] strs) {
+        int res = 0;
+        if (strs == null || strs.length == 0) return "";
+        while (true) {
+            if (res >= strs[0].length()) return strs[0].substring(0, res);
+            char c = strs[0].charAt(res);
+            for (int i = 1; i < strs.length; i++) {
+                if (res >= strs[i].length() || strs[i].charAt(res) != c) {
+                    return strs[i].substring(0, res);
+                }
+            }
+            res++;
+        }
+    }
 }
--- dfs/LetterCombinationOfaPhoneNumber.java
+++ dfs/LetterCombinationOfaPhoneNumber.java
@@ -4,36 +4,42 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
+ * Given a string containing digits from 2-9 inclusive, return all possible letter combinations that the number could represent.
+ * A mapping of digit to letters (just like on the telephone buttons) is given below. Note that 1 does not map to any letters.
+ * Example:
+ * Input: "23"
+ * Output: ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"].
+ * Note:
+ * Although the above answer is in lexicographical order, your answer could be in any order you want.
  * Created by DrunkPiano on 09/06/2017.
+ * 20200101 --review
  */
 
 public class LetterCombinationOfaPhoneNumber {
-    private static final String[] KEYS = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
 
     /**
+     * 题意：给你一串数字代表电话号码按键顺序，求所有可能的字母组合。
+     * 解法：dfs。
      * 经典组合题，类似combinations，但又不同。要看到如何开启递归。
      * 类似题目1087 BraceExpansion
      */
+    String[] s = new String[]{"abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+
     public List<String> letterCombinations(String digits) {
         List<String> res = new ArrayList<>();
-        dfs(res, digits, 0, "");
+        if (digits == null || digits.length() == 0) return res;
+        dfs(res, "", 0, digits);
         return res;
     }
 
-    private void dfs(List<String> res, String digits, int index, String item) {
-        if (index == digits.length()) {
-            res.add(item);
+    private void dfs(List<String> res, String cell, int i, String digits) {
+        if (i == digits.length()) {//已犯错误：这儿写成了i == cell.length()
+            res.add(cell);
             return;
         }
-
-        String s = KEYS[digits.charAt(index) - '0'];
-        for (int i = 0; i < s.length(); i++) {
-            dfs(res, digits, index + 1, item + s.charAt(i));
+        String pattern = s[digits.charAt(i) - '0' - 2];
+        for (int j = 0; j < pattern.length(); j++) {
+            dfs(res, cell + pattern.charAt(j), i + 1, digits);
         }
-    }
-
-    public static void main(String args[]) {
-        LetterCombinationOfaPhoneNumber lett = new LetterCombinationOfaPhoneNumber();
-        System.out.println(lett.letterCombinations("234"));
     }
 }
