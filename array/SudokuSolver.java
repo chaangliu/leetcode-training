1152dbe9d1dcf367c9269500036e3019f9c6aa26
chaangliu <chaangliu@tencent.com>
2020-01-07 13:50:33 +0800

    Sudoku Solver --review

--- array/SudokuSolver.java
+++ array/SudokuSolver.java
@@ -1,181 +1,110 @@
 package array;
 
+import java.util.Arrays;
+import java.util.HashSet;
+
 /**
+ * Write a program to solve a Sudoku puzzle by filling the empty cells.
+ * <p>
+ * A sudoku solution must satisfy all of the following rules:
+ * <p>
+ * Each of the digits 1-9 must occur exactly once in each row.
+ * Each of the digits 1-9 must occur exactly once in each column.
+ * Each of the the digits 1-9 must occur exactly once in each of the 9 3x3 sub-boxes of the grid.
+ * Empty cells are indicated by the character '.'.
+ * A sudoku puzzle...
+ * ...and its solution numbers marked in red.
+ * Note:
+ * The given board contain only digits 1-9 and the character '.'.
+ * You may assume that the given Sudoku puzzle will have a single unique solution.
+ * The given board size is always 9x9.
  * Created by DrunkPiano on 2017/4/13.
+ * 20200106 --review
  */
 
 public class SudokuSolver {
-	char[][] res;
 
-	public void solveSudoku(char[][] board) {
-		helper(board, 0, 0);
-		board = res;
-	}
+    /**
+     * 题意：解决数独问题，把board中代表空白的点填满。
+     * 解法：backtracking，技巧是用idx递增，判断valid的时候如果直接用isValidSudoku来解决，简单是简单，但是会TLE。所以我同时用了hashset，但是这样写起来很麻烦，很容易出错。
+     * 看了下别人的答案，他们是每次加入一个数之前，先去在原来的board里检查，这样就无需操作hashset了。
+     */
+    HashSet[] rows = new HashSet[9], cols = new HashSet[9], blocks = new HashSet[9];
 
-	//june 22
-	private void helper(char[][] board, int i, int j) {
-		if (j >= 9) {
-			helper(board, i + 1, 0);
-			return;
-		}
-		if (i == 9) {
-			res = board;
-			return;
-		}
-		if (board[i][j] == '.') {
-			for (int k = 1; k <= 9; k++) {
-				if (res != null) return;
-				board[i][j] = (char) (k + '0');
-				if (isValid(board, i, j)) {
-					helper(board, i, j + 1);
-				}
-				board[i][j] = '.';
-			}
-		} else {
-			helper(board, i, j + 1);
-		}
-	}
+    public void solveSudoku(char[][] A) {
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                char c = A[i][j];
+                if (rows[i] == null) rows[i] = new HashSet();
+                if (cols[j] == null) cols[j] = new HashSet();
+                if (blocks[i / 3 * 3 + j / 3] == null) blocks[i / 3 * 3 + j / 3] = new HashSet();
+                if (c != '.') {
+                    rows[i].add(c);
+                    cols[j].add(c);
+                    blocks[i / 3 * 3 + j / 3].add(c);
+                }
+            }
+        }
+        dfs(A, 0);
+    }
 
-	private boolean isValid(char[][] board, int i, int j) {
-		for (int k = 0; k < 9; k++) {
-			if (k != j && board[i][k] == board[i][j])
-				return false;
-		}
-		for (int k = 0; k < 9; k++) {
-			if (k != i && board[k][j] == board[i][j])
-				return false;
-		}
-		for (int row = i / 3 * 3; row < i / 3 * 3 + 3; row++) {
-			for (int col = j / 3 * 3; col < j / 3 * 3 + 3; col++) {
-				if ((row != i || col != j) && board[row][col] == board[i][j])
-					return false;
-			}
-		}
-		return true;
-	}
-
-
-//    //1 为什么boolean而不是void  2. 为什么i+1 而不是i++ 3.为什么恢复现场
-//    private boolean dfs(char[][] board, int i, int j) {
-//        if (i == 9) return true;
-//        if (j == 9) {
-//            return dfs(board, ++i, 0);
-//        }
-//
-////        这里没有用到双重for循环遍历棋盘，而是采用了把坐标作为参数的方式
-//        if (board[i][j] == '.') {
-//            for (int k = 1; k <= 9; k++) {
-//                board[i][j] = (char) (k + '0');
-//                //先把数字填进去，如果valid的话，就下一个格子；并且如果下一个格子也可以的话，就不把当前的格子还原了，否则还原
-//                if (isValidSudoku(board)) {
-//                    if (dfs(board, i, j + 1)) return true;
-//                }
-//                //4. 为什么这里不能用else包：
-//                board[i][j] = '.';//这里还是写到if里面去比较好理解
-//            }
-//        } else {
-//            return dfs(board, i, j + 1);
-//        }
-//        return false;
-//    }
-//
-//    public boolean isValidSudoku(char[][] board) {
-//        if (board.length != 9 || board[0].length != 9) return false;
-//        int[] temp = new int[9];
-//        //判断每一行
-//        for (int i = 0; i < 9; i++) {
-//            for (int j = 0; j < 9; j++) {
-//                if (board[i][j] != '.') {
-//                    if (temp[board[i][j] - '1'] != 0) {
-//                        return false;
-//                    }
-//                    temp[board[i][j] - '1'] = 1;
-//                }
-//            }
-//            temp = new int[9];
-//        }
-//
-//        //判断每一列
-//        for (int i = 0; i < 9; i++) {
-//            for (int j = 0; j < 9; j++) {
-//                if (board[j][i] != '.') {
-//                    if (temp[board[j][i] - '1'] != 0) {
-//                        return false;
-//                    }
-//                    temp[board[j][i] - '1'] = 1;
-//                }
-//            }
-//            temp = new int[9];
-//        }
-//
-//        //判断9个小units
-//        for (int block = 0; block < 9; block++) {
-//            for (int i = block / 3 * 3; i < block / 3 * 3 + 3; i++) {
-//                for (int j = block % 3 * 3; j < block % 3 * 3 + 3; j++) {
-//                    if (board[i][j] != '.') {
-//                        if (temp[board[i][j] - '1'] != 0) {
-//                            return false;
-//                        }
-//                        temp[board[i][j] - '1'] = 1;
-//                    }
-//                }
-//            }
-//            temp = new int[9];
-//        }
-//        return true;
-//
-//    }
-//
-//
-//    private boolean helper(char[][] board, int position) {
-//        if (position == 81) return true;
-//        int row = position / 9;
-//        int col = position % 9;
-//
-//        if (board[row][col] == '.') {
-//            for (char i = '1'; i < '9'; i++) {
-//                board[row][col] = i;
-//                //如果这个数字不会让棋盘坏掉，就用dfs插下一个数字
-//                if (checkValid(board, position, i)) {
-//                    //如果这样下去可以构造成一个pos == 80的完整board的话，就不用恢复现场了
-//                    if (helper(board, position + 1)) {
-//                        return true;
-//                    } else {
-//                        board[row][col] = '.';
-//                    }
-//                }
-//            }
-//        } else {
-////            if (helper(board, position + 1)) return true;
-//            return helper(board, position + 1);
-//        }
-//
-//        //return false是因为 return true的终止条件已经写在最前面了
-//        return false;
-//    }
-
-	private boolean checkValid(char[][] board, int position, char ins) {
-		int row = position / 9;
-		int col = position % 9;
-
-		for (int i = 0; i < 9; i++) {
-			if (i != row && board[i][col] == ins)
-				return false;
-
-		}
-
-		for (int i = 0; i < 9; i++) {
-			if (i != col && board[row][i] == ins)
-				return false;
-		}
-
-		for (int i = row / 3 * 3; i < row / 3 * 3 + 3; i++)
-			for (int j = col / 3 * 3; j < col / 3 * 3 + 3; j++) {
-//                i == row && j == col的否命题是(i != row || j != col)，同时注意||优先级小于&&
-				if ((i != row || j != col) && board[i][j] == ins) return false;
-			}
-		return true;
-	}
+    private boolean dfs(char[][] A, int idx) {
+        //if (!isValidSudoku(A)) return false; // 如果直接用isValidSudoku那一题来解决，简单是简单，但是会TLE
+        if (idx == 81) return true;
+        int row = idx / 9, col = idx % 9, g = row / 3 * 3 + col / 3;
+        if (A[row][col] != '.') {
+            if (dfs(A, idx + 1)) return true;
+        } else
+            for (int i = 1; i <= 9; i++) {
+                char newChar = (char) (i + '0');
+                if (rows[row].contains(newChar) || cols[col].contains(newChar) || blocks[g].contains(newChar)) continue;
+                A[row][col] = newChar;
+                rows[row].add(newChar);
+                cols[col].add(newChar);
+                blocks[g].add(newChar);
+                if (dfs(A, idx + 1)) return true;
+                A[row][col] = '.';
+                rows[row].remove(newChar);
+                cols[col].remove(newChar);
+                blocks[g].remove(newChar);
+            }
+        return false;
+    }
 
 
+    /**
+     * 摘抄一个答案，我看了一圈，觉得大家的答案都不太好，好多人在递归里用两层for循环来枚举。。
+     * 这个答案思路还比较清晰，用visited来标识
+     */
+    public void solveSudoku_(char[][] board) {
+        dfs___(board, 0);
+    }
+
+    private boolean dfs___(char[][] board, int d) {
+        if (d == 81) return true; //found solution
+        int i = d / 9, j = d % 9;
+        if (board[i][j] != '.') return dfs(board, d + 1);//prefill number skip
+
+        boolean[] flag = new boolean[10];
+        validate(board, i, j, flag);
+        for (int k = 1; k <= 9; k++) {
+            if (flag[k]) {
+                board[i][j] = (char) ('0' + k);
+                if (dfs___(board, d + 1)) return true;
+            }
+        }
+        board[i][j] = '.'; //if can not solve, in the wrong path, change back to '.' and out
+        return false;
+    }
+
+    private void validate(char[][] board, int i, int j, boolean[] flag) {
+        Arrays.fill(flag, true);
+        for (int k = 0; k < 9; k++) {
+            if (board[i][k] != '.') flag[board[i][k] - '0'] = false;
+            if (board[k][j] != '.') flag[board[k][j] - '0'] = false;
+            int r = i / 3 * 3 + k / 3;
+            int c = j / 3 * 3 + k % 3;
+            if (board[r][c] != '.') flag[board[r][c] - '0'] = false;
+        }
+    }
 }
--- dfs/ValidSudoku.java
+++ dfs/ValidSudoku.java
@@ -1,11 +1,81 @@
 package dfs;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
+ * Determine if a 9x9 Sudoku board is valid. Only the filled cells need to be validated according to the following rules:
+ * Each row must contain the digits 1-9 without repetition.
+ * Each column must contain the digits 1-9 without repetition.
+ * Each of the 9 3x3 sub-boxes of the grid must contain the digits 1-9 without repetition.
+ * A partially filled sudoku which is valid.
+ * The Sudoku board could be partially filled, where empty cells are filled with the character '.'.
+ * Example 1:
+ * Input:
+ * [
+ * ["5","3",".",".","7",".",".",".","."],
+ * ["6",".",".","1","9","5",".",".","."],
+ * [".","9","8",".",".",".",".","6","."],
+ * ["8",".",".",".","6",".",".",".","3"],
+ * ["4",".",".","8",".","3",".",".","1"],
+ * ["7",".",".",".","2",".",".",".","6"],
+ * [".","6",".",".",".",".","2","8","."],
+ * [".",".",".","4","1","9",".",".","5"],
+ * [".",".",".",".","8",".",".","7","9"]
+ * ]
+ * Output: true
+ * Example 2:
+ * <p>
+ * Input:
+ * [
+ * ["8","3",".",".","7",".",".",".","."],
+ * ["6",".",".","1","9","5",".",".","."],
+ * [".","9","8",".",".",".",".","6","."],
+ * ["8",".",".",".","6",".",".",".","3"],
+ * ["4",".",".","8",".","3",".",".","1"],
+ * ["7",".",".",".","2",".",".",".","6"],
+ * [".","6",".",".",".",".","2","8","."],
+ * [".",".",".","4","1","9",".",".","5"],
+ * [".",".",".",".","8",".",".","7","9"]
+ * ]
+ * Output: false
+ * Explanation: Same as Example 1, except with the 5 in the top left corner being
+ * modified to 8. Since there are two 8's in the top left 3x3 sub-box, it is invalid.
+ * Note:
+ * <p>
+ * A Sudoku board (partially filled) could be valid but is not necessarily solvable.
+ * Only the filled cells need to be validated according to the mentioned rules.
+ * The given board contain only digits 1-9 and the character '.'.
+ * The given board size is always 9x9.
  * Created by DrunkPiano on 2017/4/13.
+ * 20190107 --review
  */
 
 public class ValidSudoku {
+    /**
+     * 题意：给你一个board，里面可能有0-9，或者点(代表empty)，问你这个board是否是一个合理的数独board。
+     * 今天又做这道题，题目没读懂，把它当成sudoku solver那题了，我还在想怎么这题变成Medium了。
+     * 然后看了讨论区的答案，发现Stefan Porchman非常巧妙的one pass解法，encode + hashset
+     */
     public boolean isValidSudoku(char[][] board) {
+        Set seen = new HashSet();
+        for (int i = 0; i < 9; ++i) {
+            for (int j = 0; j < 9; ++j) {
+                char number = board[i][j];
+                if (number != '.')
+                    if (!seen.add(number + " in row " + i) ||
+                            !seen.add(number + " in column " + j) ||
+                            !seen.add(number + " in block " + i / 3 + "-" + j / 3))
+                        return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * 原始解法
+     */
+    public boolean isValidSudoku__(char[][] board) {
         if (board.length != 9 || board[0].length != 9) return false;
         int[] temp = new int[9];
         //判断每一行
@@ -50,57 +120,5 @@ public class ValidSudoku {
         }
         return true;
 
-    }
-
-//    private boolean checkValid(char board[][]) {
-////        HashMap map = new HashMap();
-////        int[] temp = {-1, -1, -1, -1, -1, -1, -1, -1, -1};
-//        int[] temp = new int[9];
-//        //判断每一行
-//        for (int i = 0; i < 9; i++) {
-//            for (int j = 0; j < 9; j++) {
-//                if (board[i][j] != '.') {
-//                    if (temp[board[i][j] - '1'] != 0) {
-//                        return false;
-//                    }
-//                    temp[board[j][i] - '1'] = 1;
-//                }
-//            }
-//            temp = new int[9];
-//        }
-//
-//        //判断每一列
-//        for (int i = 0; i < 9; i++) {
-//            for (int j = 0; j < 9; j++) {
-//                if (board[j][i] != '.') {
-//                    if (temp[board[j][i] - '1'] != 0) {
-//                        return false;
-//                    }
-//                    temp[board[j][i] - '1'] = 1;
-//                }
-//            }
-//            temp = new int[9];
-//        }
-//
-//        //判断9个小units
-//        for (int block = 0; block < 9; block++) {
-//            for (int i = block / 3 * 3; i < block / 3 * 3 + 3; i++) {
-//                for (int j = block % 3; j < block % 3 + 3; j++) {
-//                    if (board[i][j] != '.') {
-//                        if (temp[board[i][j] - '0'] != 0) {
-//                            return false;
-//                        }
-//                        temp[board[i][j]] = 1;
-//                    }
-//                }
-//            }
-//            temp = new int[9];
-//        }
-//        return true;
-//    }
-
-    public static void main(String args[]) {
-        char i = 9;
-        System.out.println(i - 1);
     }
 }
