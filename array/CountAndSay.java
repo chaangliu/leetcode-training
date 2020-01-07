ae7f26c94588f58666eafe234f152ee5a693b92f
chaangliu <chaangliu@tencent.com>
2020-01-07 22:05:17 +0800

    38. Count and Say

--- array/CountAndSay.java
+++ array/CountAndSay.java
@@ -1,42 +1,66 @@
 package array;
 
 /**
- * 1, 11, 21, 1211, 111221, ...
- * Given an integer n, generate the nth sequence.
+ * The count-and-say sequence is the sequence of integers with the first five terms as following:
+ * 1.     1
+ * 2.     11
+ * 3.     21
+ * 4.     1211
+ * 5.     111221
+ * 1 is read off as "one 1" or 11.
+ * 11 is read off as "two 1s" or 21.
+ * 21 is read off as "one 2, then one 1" or 1211.
+ * <p>
+ * Given an integer n where 1 ≤ n ≤ 30, generate the nth term of the count-and-say sequence. You can do so recursively, in other words from the previous member read off the digits, counting the number of digits in groups of the same digit.
+ * <p>
+ * Note: Each term of the sequence of integers will be represented as a string.
  * <p>
  * <p>
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: 1
+ * Output: "1"
+ * Explanation: This is the base case.
+ * Example 2:
+ * <p>
+ * Input: 4
+ * Output: "1211"
+ * Explanation: For n = 3 the term was "21" in which we have two groups "2" and "1", "2" can be read as "12" which means frequency = 1 and value = 2, the same way "1" is read as "11", so the answer is the concatenation of "12" and "11" which is "1211".
  * Created by DrunkPiano on 2017/3/18.
+ * 20200107 --review
  */
 
 public class CountAndSay {
+    /**
+     * 题意：按照要求读出字符串，比如1112就读成3112，也就是连续的数字计算出数量。
+     * 1.     1
+     * 2.     11
+     * 3.     21
+     * 4.     1211
+     * 5.     111221
+     * 6.     312211
+     * 7.     13112221
+     * 8.     1113213211
+     * 9.     31131211131221
+     * 10.     13211311123113112211
+     * 我这个答案偷懒没有用StringBuilder，还算简洁
+     */
     public String countAndSay(int n) {
-        StringBuilder res = new StringBuilder();
-        res.append("1");
-        int count = 1;
-        int i = 1;
-        while (i < n) {
-            StringBuilder sb = new StringBuilder();
-            for (int j = 1; j < res.length(); j++) {
-                if (res.charAt(j) == res.charAt(j - 1)) {
-                    count++;
-                } else {
-                    sb.append(count);
-                    sb.append(res.charAt(j - 1));
-                    count = 1;
+        String prev = "1";
+        String cur = "";
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < prev.length(); j++) {
+                int cnt = 1;
+                while (j + 1 < prev.length() && prev.charAt(j) == prev.charAt(j + 1)) {
+                    cnt++;
+                    j++;
                 }
+                cur += cnt + "" + prev.charAt(j);
             }
-            sb.append(count);
-            //count 每次都要置1
-            count = 1;
-            sb.append(res.charAt(res.length() - 1));
-            res = sb;
-            i++;
+            prev = cur;
+            cur = "";// 已犯错误：忘记把cur置空
         }
-        return res.toString();
-    }
-
-    public static void main(String args[]) {
-        CountAndSay countAndSay = new CountAndSay();
-        System.out.println(countAndSay.countAndSay(5));
+        return prev;
     }
 }
