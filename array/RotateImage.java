ec62e3bb02b5246fefa402081dc2eba40c1131d7
chaangliu <chaangliu@tencent.com>
2020-01-11 12:42:50 +0800

    48. Rotate Image --review

--- array/RotateImage.java
+++ array/RotateImage.java
@@ -9,20 +9,32 @@ package array;
  * Could you do this in-place?
  * <p>
  * Created by DrunkPiano on 2017/1/5.
+ * 20200111 --review
  */
 
 public class RotateImage {
 
     /**
-     * step1: reverse through the anticlockwise diagonal line
-     * step2: reverse through the middle line of y axis
-     *
-     * @param matrix the matrix to rotate
+     * 题意：顺时针旋转图像90度。要求inplace。
+     * 解法：这题不是很intuitive，所以想不出来很正常。
+     * 要记住。通用解法是先垂直对称再对角线对称。
+     * 顺时针逆时针取决于上下翻转还是左右翻转。
+     * <p>
+     * clockwise rotate
+     * first reverse up to down, then swap the symmetry
+     * 1 2 3     7 8 9     7 4 1
+     * 4 5 6  => 4 5 6  => 8 5 2
+     * 7 8 9     1 2 3     9 6 3
+     * <p>
+     * anticlockwise rotate
+     * first reverse left to right, then swap the symmetry
+     * 1 2 3     3 2 1     3 6 9
+     * 4 5 6  => 6 5 4  => 2 5 8
+     * 7 8 9     9 8 7     1 4 7
      */
     public void rotate(int[][] matrix) {
         int temp;
         int n = matrix.length;
-        if (n < 0) return;
         for (int i = 0; i < n - 1; i++) {
             for (int j = 0; j < n - i; j++) {
                 temp = matrix[n - 1 - j][n - 1 - i];
@@ -36,25 +48,6 @@ public class RotateImage {
                 temp = matrix[i][j];
                 matrix[i][j] = matrix[n - 1 - i][j];
                 matrix[n - i][j] = temp;
-            }
-        }
-    }
-
-    //    public static void main(String args[]) {
-////        int[][] matrix = {{1, 2, 3, 4}, {3, 4, 4}, {3}, {3}, {3}, {3}, {3}, {3}, {3}};
-//        int[][] matrix = {{1,2},{3,4}};
-//        RotateImage rotateImage = new RotateImage();
-//        rotateImage.rotate(matrix);
-////        System.out.println(rotateImage.n);
-//    }
-    public static void main(String args[]) {
-//        int a[][] = {{1, 2}, {3, 4, 5, 6}, {7, 8, 9}};
-        int a[][] = {{1, 2}, {3, 4}};
-        RotateImage rotateImage = new RotateImage();
-        rotateImage.rotate(a);
-        for (int i = 0; i < a.length; i++) {
-            for (int j = 0; j < a[i].length; j++) {
-                System.out.println("a[" + i + "][" + j + "]=" + a[i][j]);
             }
         }
     }
--- greedy/NextPermutation.java
+++ greedy/NextPermutation.java
@@ -1,12 +1,20 @@
 package greedy;
 
 /**
- * NextPermutation.
+ * Implement next permutation, which rearranges numbers into the lexicographically next greater permutation of numbers.
+ * If such arrangement is not possible, it must rearrange it as the lowest possible order (ie, sorted in ascending order).
+ * The replacement must be in-place and use only constant extra memory.
+ * Here are some examples. Inputs are in the left-hand column and its corresponding outputs are in the right-hand column.
+ * 1,2,3 → 1,3,2
+ * 3,2,1 → 1,2,3
+ * 1,1,5 → 1,5,1
  * Created by DrunkPiano on 2016/12/22.
+ * 201200111 --review
  */
 
 public class NextPermutation {
     /**
+     * 题意：找到当前数组
      * 20190526review 今天遇到1053. Previous Permutation With One Swap过来回顾一下；
      * 这题不用递归，思路也比较直观，从后往前找一个递增的pair，然后把pair[0]和它之后所有数里最接近的那个数交换位置，最后把pair[0]后面的数reverse一下。需要纸上画一画。
      */
@@ -15,7 +23,7 @@ public class NextPermutation {
         //1.find the last number which breaks the reversal increasing order
         int pos = -1;
         for (int i = num.length - 1; i > 0; i--) {
-            if (num[i] > num[i - 1]) {
+            if (num[i] > num[i - 1]) { // [1 2 4 6 5 3] => 4
                 pos = i - 1;
                 break;
             }
@@ -27,9 +35,9 @@ public class NextPermutation {
             return;
         }
 
-        //3. find the last number which is bigger than i
+        //3. find the last number which is bigger than i，注意是last，也就是从后往前第一个大于num[pos]的数
         for (int i = num.length - 1; i > 0; i--) {
-            if (num[i] > num[pos]) {
+            if (num[i] > num[pos]) { // [1 2 5 6 4 3]
                 //swap position and this number
                 int temp = num[pos];
                 num[pos] = num[i];
@@ -39,7 +47,7 @@ public class NextPermutation {
         }
 
         //4.反排pos之后的数
-        reverse(num, pos + 1, num.length - 1);
+        reverse(num, pos + 1, num.length - 1);// [1 2 5 3 4 6]
     }
 
     public static void reverse(int[] num, int begin, int end) {
