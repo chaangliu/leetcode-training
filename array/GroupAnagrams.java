4d3df7c9f80b4b2c9ff8f49f5459fcba7d764b30
chaangliu <chaangliu@tencent.com>
2020-01-11 12:55:15 +0800

    49. Group Anagrams --review

--- array/GroupAnagrams.java
+++ array/GroupAnagrams.java
@@ -3,79 +3,62 @@ package array;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
 /**
- * Created by DrunkPiano on 24/05/2017.
- * for:
- * ["eat", "tea", "tan", "ate", "nat", "bat"]
- * return:
+ * Given an array of strings, group anagrams together.
+ * Example:
+ * <p>
+ * Input: ["eat", "tea", "tan", "ate", "nat", "bat"],
+ * Output:
  * [
- * ["ate", "eat","tea"],
+ * ["ate","eat","tea"],
  * ["nat","tan"],
  * ["bat"]
  * ]
+ * Note:
+ * <p>
+ * All inputs will be in lowercase.
+ * The order of your output does not matter.
+ * Created by DrunkPiano on 24/05/2017.
+ * 20200111 --review
  */
 
 public class GroupAnagrams {
-//	public List<List<String>> groupAnagrams(String[] strs) {
-//		Map<int[], List<String>> hashMap = new HashMap<>();
-//		for (String s : strs) {
-//			boolean addedToExist = false ;
-//			int[] map = new int[26];
-//			for (int i = 0; i < s.length(); i++) {
-//				map[s.charAt(i) - 'a']++;
-//			}
-//			if (hashMap.isEmpty()) {
-//				hashMap.put(map, new ArrayList<String>());
-//			}
-//			Iterator iter = hashMap.entrySet().iterator();
-//			while (iter.hasNext()) {
-//				Map.Entry entry = (Map.Entry) iter.next();
-//				int[] key = (int[]) entry.getKey();
-//				if (Arrays.equals(key, map)) {
-//					List<String> list = hashMap.get(entry.getKey());
-//					list.add(s);
-//					addedToExist = true;
-////					hashMap.put(key, list);//这里是否不需要put，因为list是引用
-//				}
-//			}
-//			if (!addedToExist){
-//				List<String> list = new ArrayList<>();
-//				list.add(s);
-//				hashMap.put(map , list);
-//			}
-//
-//		}
-//
-//		List<List<String>> res = new ArrayList<>();
-//		Iterator iter = hashMap.entrySet().iterator();
-//		while (iter.hasNext()) {
-//			Map.Entry<int[], ArrayList<String>> entry = (Map.Entry<int[], ArrayList<String>>) iter.next();
-//			List<String> list = entry.getValue();
-//			res.add(list);
-//		}
-//		return res;
-//	}
+    /**
+     * 题意：把anagram归类，顺序没关系。
+     * 解法：最intuitive的解法就是sort + map，如下。
+     */
+    public List<List<String>> groupAnagrams(String[] strs) {
+        if (strs == null || strs.length == 0) return new ArrayList<>();
+        Map<String, List<String>> map = new HashMap<>();
+        for (String s : strs) {
+            char[] ca = s.toCharArray(); //先转换为charArray再sort
+            Arrays.sort(ca);
+            String keyStr = String.valueOf(ca);// String.valueOf(char [])
+            if (!map.containsKey(keyStr)) map.put(keyStr, new ArrayList<>());
+            map.get(keyStr).add(s);
+        }
+        return new ArrayList<>(map.values());
+    }
 
-	public List<List<String>> groupAnagrams(String[] strs) {
-		if (strs == null || strs.length == 0 ) return new ArrayList<>();
-		Map<String , List<String>> map = new HashMap<>();
-		for (String s : strs){
-			char [] c = s.toCharArray();
-			Arrays.sort(c);
-			String key = String.copyValueOf(c);
-			if (!map.containsKey(key)){
-				map.put(key, new ArrayList<String>());
-			}
-			map.get(key).add(s);
-		}
-		return new ArrayList<>(map.values());
-	}
-
-		public static void main(String args[]) {
-		String[] s = {"eat", "tea", "tan", "ate", "nat", "bat"};
-		System.out.println(new GroupAnagrams().groupAnagrams(s));
-	}
+    /**
+     * O(m*n)做法，借助一个int [26]来编码
+     */
+    public List<List<String>> groupAnagrams_(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+        for (String s : strs) {
+            int[] arr = new int[26];
+            for (int i = 0; i < s.length(); i++) {
+                arr[s.charAt(i) - 'a']++;
+            }
+            String key = Arrays.toString(arr);
+            List<String> tempList = map.getOrDefault(key, new LinkedList<String>());
+            tempList.add(s);
+            map.put(key, tempList);
+        }
+        return new LinkedList<>(map.values());
+    }
 }
