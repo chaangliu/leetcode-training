ec6d431acb2c7edacf35512df2fea7626323d348
chaangliu <chaangliu@tencent.com>
2020-03-09 18:29:03 +0800

    stock review

--- dp/BestTimetoBuyandSellStock.java
+++ dp/BestTimetoBuyandSellStock.java
@@ -37,6 +37,21 @@ public class BestTimetoBuyandSellStock {
         return global;
     }
 
+    /**
+     * 解法2：寻找最大差值
+     */
+    public int maxProfit_(int prices[]) {
+        int minprice = Integer.MAX_VALUE;
+        int maxprofit = 0;
+        for (int i = 0; i < prices.length; i++) {
+            if (prices[i] < minprice)
+                minprice = prices[i];
+            else if (prices[i] - minprice > maxprofit)
+                maxprofit = prices[i] - minprice;
+        }
+        return maxprofit;
+    }
+
 
     public static void main(String args[]) {
         BestTimetoBuyandSellStock instance = new BestTimetoBuyandSellStock();
