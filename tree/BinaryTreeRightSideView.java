01b6a47965ec71fee3a0c8d0696f8276ec20cd18
chaangliu <chaangliu@tencent.com>
2020-04-22 11:06:22 +0800

    binary tree right side view --review

--- tree/BinaryTreeRightSideView.java
+++ tree/BinaryTreeRightSideView.java
@@ -4,50 +4,43 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
+ * 给定一棵二叉树，想象自己站在它的右侧，按照从顶部到底部的顺序，返回从右侧所能看到的节点值。
+ *
+ * 示例:
+ *
+ * 输入: [1,2,3,null,5,null,4]
+ * 输出: [1, 3, 4]
+ * 解释:
+ *
+ *    1            <---
+ *  /   \
+ * 2     3         <---
+ *  \     \
+ *   5     4       <---
+ *
  * Created by DrunkPiano on 2017/4/24.
  */
 
 public class BinaryTreeRightSideView {
+    /**
+     * 题意：给定一棵二叉树，想象自己站在它的右侧，按照从顶部到底部的顺序，返回从右侧所能看到的节点值。
+     * 解法：DFS、BFS。
+     * DFS可以先遍历右边子树，这样每当size == level就添加即可；
+     * BFS由于每一层最后一个访问到的节点才是我们要的答案，因此不断更新对应深度的信息即可。
+     */
     public List<Integer> rightSideView(TreeNode root) {
         List<Integer> res = new ArrayList<>();
-        if (root == null) return res;
-//        LinkedList<TreeNode> queue = new LinkedList<>();
-//        queue.add(root);
-//        int curNum = 1;
-//        int nextNum = 0 ;
-//        while (!queue.isEmpty()) {
-//            TreeNode node = queue.poll();
-//            curNum -- ;
-//
-//            if (node.left!=null){
-//                queue.offer(node.left);
-//                nextNum ++ ;
-//            }
-//
-//            if (node.right!=null){
-//                queue.offer(node.right);
-//                nextNum ++ ;
-//            }
-//
-//            if (curNum==0){
-//                res.add(node.val);
-//                curNum = nextNum ;
-//                nextNum = 0 ;
-//            }
-//        }
-//        return res ;
-
-//        dfs(root , 0 , res);
-        return res ;
+        dfs(res, root, 0);
+        return res;
     }
 
-    private void dfs(TreeNode root, int level, ArrayList<ArrayList<Integer>> list) {
-        if (root == null) return;
-        if (level >= list.size()) {
-            list.add(new ArrayList<Integer>());
+    private void dfs(List<Integer> res, TreeNode node, int level) {
+        if(node != null) {
+            if(res.size() == level) {
+                res.add(node.val);
+            }
+            dfs(res, node.right, level + 1);
+            dfs(res, node.left, level + 1);
         }
-        list.get(level).add(root.val);
-        dfs(root.left, level + 1, list);
-        dfs(root.right, level + 1, list);
     }
 }
