01b514d71cbff27360b14f1805097742317aee68
chaangliu <chaangliu@tencent.com>
2020-05-30 14:22:09 +0800

    874. Walking Robot Simulation

---
+++ easy/WalkingRobotSimulation.java
@@ -0,0 +1,62 @@
+package easy;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 机器人在一个无限大小的网格上行走，从点 (0, 0) 处开始出发，面向北方。该机器人可以接收以下三种类型的命令：
+ * -2：向左转 90 度
+ * -1：向右转 90 度
+ * 1 <= x <= 9：向前移动 x 个单位长度
+ * 在网格上有一些格子被视为障碍物。
+ * <p>
+ * 第 i 个障碍物位于网格点  (obstacles[i][0], obstacles[i][1])
+ * <p>
+ * 机器人无法走到障碍物上，它将会停留在障碍物的前一个网格方块上，但仍然可以继续该路线的其余部分。
+ * <p>
+ * 返回从原点到机器人所有经过的路径点（坐标为整数）的最大欧式距离的平方。
+ * 示例 1：
+ * 输入: commands = [4,-1,3], obstacles = []
+ * 输出: 25
+ * 解释: 机器人将会到达 (3, 4)
+ * 示例 2：
+ * <p>
+ * 输入: commands = [4,-1,4,-2,4], obstacles = [[2,4]]
+ * 输出: 65
+ * 解释: 机器人在左转走到 (1, 8) 之前将被困在 (1, 4) 处
+ * 20200530
+ */
+public class WalkingRobotSimulation {
+    /**
+     * 题意：模拟机器人行走，问距离起点(0,0)最远的欧拉距离。
+     * 解法：模拟，要点是：1. 用set保存encode后的obstacles 2. 用dirs记录上右下左方向
+     */
+    public int robotSim(int[] commands, int[][] obstacles) {
+        Set<String> set = new HashSet<>();
+        for (int[] obs : obstacles) {
+            set.add(obs[0] + " " + obs[1]);
+        }
+        int[][] dirs = new int[][]{{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+        int d = 0, x = 0, y = 0, result = 0;
+        for (int c : commands) {
+            if (c == -1) {
+                d++;
+                if (d == 4) {
+                    d = 0;
+                }
+            } else if (c == -2) {
+                d--;
+                if (d == -1) {
+                    d = 3;
+                }
+            } else {
+                while (c-- > 0 && !set.contains((x + dirs[d][0]) + " " + (y + dirs[d][1]))) {
+                    x += dirs[d][0];
+                    y += dirs[d][1];
+                }
+            }
+            result = Math.max(result, x * x + y * y);
+        }
+        return result;
+    }
+}
