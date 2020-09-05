8d52fff0d6071357d190a8ac097c03ca9c990011
chaangliu <chaangliu@tencent.com>
2020-09-05 22:22:48 +0800

    60. Permutation Sequence review

--- dfs/PermutationSequence.java
+++ dfs/PermutationSequence.java
@@ -5,63 +5,65 @@ package dfs;
  */
 
 public class PermutationSequence {
-//	String res = "";
-//
-//	public String getPermutation(int n, int k) {
-//		int[] count = new int[1];
-//		boolean[] used = new boolean[n + 1];
-//		dfs(n, k, count, used, "", 1);
-//		return res;
-//	}
-//
-//	private String dfs(int n, int k, int[] count, boolean[] used, String item, int arrayIndex) {
-//		if (item.length() == n) {
-//			count[0]++;
-//			return null;
-//		}
-//		if (k == count[0]) {
-//			res = item;
-////			return item;
-//		}
-//		for (int i = arrayIndex; i <= n; i++) {
-//			if (!used[i]) {
-//				used[i] = true;
-//				dfs(n, k, count, used, item + i, arrayIndex + 1);
-//				used[i] = false;
-//			}
-//		}
-//		return null;
-//	}
+    /**
+     * 题意：给你n个数字，求第k个permutation。
+     * 解法：Math没看，暴力permutation也能过。
+     */
+    class Solution {
+        int k = 0;
 
-	String res = "";
-	public String getPermutation(int n, int k) {
-		int[] count = new int[1];
-		int[] num = new int[n];
-		for (int i = 0; i < n; i++) {
-			num[i] = i + 1;
-		}
-		backtracking(count, k, num, "", new boolean[num.length]);
-		return res;
+        public String getPermutation(int n, int k) {
+            this.k = k;
+            return permute("", n, new boolean[n + 1]);
+        }
 
-	}
+        private String permute(String cur, int n, boolean[] visited) {
+            if (cur.length() == n) {
+                if (--k == 0) {
+                    return cur;
+                } else return "";
+            }
+            for (int i = 1; i <= n; i++) {
+                if (visited[i]) continue;
+                visited[i] = true;
+                String r = permute(cur + i, n, visited);
+                if (r.length() != 0) return r;
+                visited[i] = false;
+            }
+            return "";
+        }
+    }
 
-	private void backtracking(int[] count, int k, int[] nums, String item, boolean[] used) {
-		if (item.length() == nums.length) {
-			if (++count[0] == k) {
-				res = item;
-			}
-			return;
-		}
-		for (int i = 0; i < nums.length; i++) {
-			if (!used[i]) {
-				used[i] = true;
-				backtracking(count, k, nums, item + nums[i], used);
-				used[i] = false;
-			}
-		}
-	}
+    String res = "";
 
-	public static void main(String args[]) {
-		System.out.println(new PermutationSequence().getPermutation(3, 5));
-	}
+    public String getPermutation(int n, int k) {
+        int[] count = new int[1];
+        int[] num = new int[n];
+        for (int i = 0; i < n; i++) {
+            num[i] = i + 1;
+        }
+        backtracking(count, k, num, "", new boolean[num.length]);
+        return res;
+
+    }
+
+    private void backtracking(int[] count, int k, int[] nums, String item, boolean[] used) {
+        if (item.length() == nums.length) {
+            if (++count[0] == k) {
+                res = item;
+            }
+            return;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (!used[i]) {
+                used[i] = true;
+                backtracking(count, k, nums, item + nums[i], used);
+                used[i] = false;
+            }
+        }
+    }
+
+    public static void main(String args[]) {
+        System.out.println(new PermutationSequence().getPermutation(3, 5));
+    }
 }
