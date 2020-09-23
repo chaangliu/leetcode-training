d6dce682745aa42b16ecd591d235911b84b923f6
chaangliu <chaangliu@tencent.com>
2020-09-23 14:14:31 +0800

    merge tow binary trees

--- tree/MergeTwoBinaryTrees.java
+++ tree/MergeTwoBinaryTrees.java
@@ -1,45 +1,40 @@
 package tree;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * Created by DrunkPiano on 11/06/2017.
  */
 
 public class MergeTwoBinaryTrees {
-	int value;
+    /**
+     * 题意：合并两棵二叉树。
+     * 解法：dfs。我写的时候因为想直接merge到t1上，所以提前判断了下一层。其实可以直接生成新node比较方便。也可以bfs，较长。
+     */
+    public TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
+        if (t1 == null) return t2;
+        if (t2 == null) return t1;
+        TreeNode root = new TreeNode(t1.val + t2.val);
+        root.left = mergeTrees(t1.left, t2.left);
+        root.right = mergeTrees(t1.right, t2.right);
+        return root;
+    }
 
-	public TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
-
-		TreeNode res = dfs(t1, t2);
-		return res;
-	}
-
-
-	private TreeNode dfs(TreeNode t1, TreeNode t2) {
-		if (t1 == null && t2 == null) {
-			return null;
-		}
-		if (t1 != null && t2 != null) {
-			value = t1.val + t2.val;
-		} else if (t2 != null) {
-			return t2;
-		} else {
-			return t1;
-		}
-		TreeNode node = new TreeNode(value);
-		node.left = mergeTrees(t1.left, t2.left);
-		node.right = mergeTrees(t1.right, t2.right);
-		return node;
-	}
-
-
-	public static void main(String args[]) {
-		TreeNode root1 = new TreeNode(1);
-		root1.left = new TreeNode(2);
-		root1.right = new TreeNode(3);
-
-		TreeNode root2 = new TreeNode(4);
-		root2.left = new TreeNode(5);
-		root2.right = new TreeNode(6);
-		new MergeTwoBinaryTrees().mergeTrees(root1, root2);
-	}
+    public TreeNode mergeTrees_(TreeNode t1, TreeNode t2) {
+        if (t1 == null) return t2;
+        if (t2 == null) return t1;
+        t1.val += t2.val;
+        if (t1.left == null) {
+            t1.left = t2.left;
+            t2.left = null;
+        }
+        if (t1.right == null) {
+            t1.right = t2.right;
+            t2.right = null;
+        }
+        mergeTrees_(t1.left, t2.left);
+        mergeTrees_(t1.right, t2.right);
+        return t1;
+    }
 }
