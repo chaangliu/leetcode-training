ff73b6eb9fb2f417d5d1260a12aec5b91765e2e0
chaangliu <chaangliu@tencent.com>
2020-10-02 12:06:15 +0800

    reverseWords

--- array/ReverseWordsInAString.java
+++ array/ReverseWordsInAString.java
@@ -12,24 +12,32 @@ package array;
  */
 
 public class ReverseWordsInAString {
+    /**
+     * 题意：翻转字符串中的单词。
+     * 解法：split肯定可以，但是双指针更好，效率更高。从尾到头搜索单词；j指向尾，i指向头。
+     * https://leetcode-cn.com/problems/fan-zhuan-dan-ci-shun-xu-lcof/solution/mian-shi-ti-58-i-fan-zhuan-dan-ci-shun-xu-shuang-z/
+     */
+    public String reverseWords_(String s) {
+        s = s.trim(); // 删除首尾空格
+        int j = s.length() - 1, i = j;
+        StringBuilder res = new StringBuilder();
+        while (i >= 0) {
+            while (i >= 0 && s.charAt(i) != ' ') i--; // 搜索首个空格
+            res.append(s.substring(i + 1, j + 1) + " "); // 添加单词
+            while (i >= 0 && s.charAt(i) == ' ') i--; // 跳过单词间空格
+            j = i; // j 指向下个单词的尾字符
+        }
+        return res.toString().trim(); // 转化为字符串并返回
+    }
+
     public String reverseWords(String s) {
         String[] array = s.split(" ");
         StringBuilder sb = new StringBuilder();
         for (int i = array.length - 1; i >= 0; i--) {
-            if (!array[i].equals("") ) {
+            if (!array[i].equals("")) { // split多个空格的时候，有可能出现空单词
                 sb.append(array[i]).append(" ");
             }
-            //这样写的话就忽略了" 1"这种情况
-            // if (i != 0) {
-            //     sb.append(" ");
-            // }
         }
         return sb.length() == 0 ? "" : sb.substring(0, sb.length() - 1);
-    }
-
-    public static void main(String args[]) {
-        ReverseWordsInAString reverseWordsInAString = new ReverseWordsInAString();
-//        System.out.println(reverseWordsInAString.reverseWords(" a"));
-        System.out.println(reverseWordsInAString.reverseWords("the sky is blue"));
     }
 }
