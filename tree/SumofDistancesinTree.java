61a70cdba7bff31734ea75666088dfde1b6a8a79
chaangliu <chaangliu@tencent.com>
2020-10-06 23:57:12 +0800

    834. Sum of Distances in Tree

--- tree/SumofDistancesinTree.java
+++ tree/SumofDistancesinTree.java
@@ -1,7 +1,9 @@
 package tree;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashSet;
+import java.util.List;
 
 /**
  * An undirected, connected tree with N nodes labelled 0...N-1 and N-1 edges are given.
@@ -28,16 +30,16 @@ import java.util.HashSet;
  * 20190614
  */
 public class SumofDistancesinTree {
+    /**
+     * 题意：给你N边，这些边组成了一棵N-1个node的多叉树；让你依次输出每个node到其他所有node的距离之和。
+     * 解法：树形dp(https://www.cnblogs.com/ZhaoxiCheung/p/LeetCode-SumofDistancesinTree.html)
+     * 先建无向图，然后post order自底向上地记录每个node的res和拥有的children数，
+     * 最后preorder自顶向下地利用一个巧妙的公式计算以每个node为root时的res。
+     * 相似题目：968、979
+     */
     int[] res, count;
     ArrayList<HashSet<Integer>> tree;
 
-    /**
-     * 一道HARD，有点像树形DP(https://www.cnblogs.com/ZhaoxiCheung/p/LeetCode-SumofDistancesinTree.html)
-     * 先建无向图，然后post order自底向上地记录每个node的res和拥有的children数，
-     * 最后preorder自顶向下地利用一个巧妙的公式计算以每个node为root时的res。
-     *
-     * 相似题目：968、979
-     */
     public int[] sumOfDistancesInTree(int N, int[][] edges) {
         tree = new ArrayList<HashSet<Integer>>();
         res = new int[N];
@@ -74,8 +76,52 @@ public class SumofDistancesinTree {
         }
     }
 
-    public static void main(String args[]) {
-        int[][] arr = new int[][]{{0, 1}, {0, 2}, {2, 3}, {2, 4}, {2, 5}};
-        new SumofDistancesinTree().sumOfDistancesInTree(6, arr);
+    /**
+     * leetcode-cn discussion
+     */
+    class Solution {
+        private List<List<Integer>> graph = new ArrayList<>();//邻接表
+        int[] distSum; //距离和
+        int[] nodeNum; //子树节点个数（包括自己）
+
+        public int[] sumOfDistancesInTree(int N, int[][] edges) {
+
+            for (int i = 0; i < N; i++) {
+                graph.add(new ArrayList<Integer>());
+            }
+            for (int i = 0; i < edges.length; i++) {
+                int src = edges[i][0];
+                int dst = edges[i][1];
+                graph.get(src).add(dst);
+                graph.get(dst).add(src);
+            }
+            distSum = new int[N];
+            nodeNum = new int[N];
+            Arrays.fill(nodeNum, 1);
+            postOrder(0, -1);
+            preOrder(0, -1);
+            return distSum;
+        }
+
+        // 求root到子树所有节点的距离和
+        private void postOrder(int root, int parent) {
+            List<Integer> neighbors = graph.get(root);
+            for (Integer neighbor : neighbors) {
+                if (neighbor == parent) continue; // 如果邻接点是父节点，则跳过
+                postOrder(neighbor, root);
+                nodeNum[root] += nodeNum[neighbor];
+                distSum[root] += distSum[neighbor] + nodeNum[neighbor];
+            }
+        }
+
+        // 根据root计算其邻居到所在子树之外的节点的距离和（包括root节点）
+        private void preOrder(int root, int parent) {
+            List<Integer> neighbors = graph.get(root);
+            for (Integer neighbor : neighbors) {
+                if (neighbor == parent) continue;
+                distSum[neighbor] = distSum[root] - nodeNum[neighbor] + (graph.size() - nodeNum[neighbor]);
+                preOrder(neighbor, root);
+            }
+        }
     }
 }
