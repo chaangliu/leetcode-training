978909c1652fd8bc59b5a2bbce4a18e08a426435
chaangliu <chaangliu@tencent.com>
2020-10-18 23:27:26 +0800

    19. Remove Nth Node From End of List

--- linkedlist/RemoveNthNodeFromEndOfList.java
+++ linkedlist/RemoveNthNodeFromEndOfList.java
@@ -18,7 +18,7 @@ public class RemoveNthNodeFromEndOfList {
      * 解法：one pass的方法就是快慢指针，但是要注意特殊case比如[1]1, [1,2]2
      */
     public ListNode removeNthFromEnd(ListNode head, int n) {
-        ListNode dummy = new ListNode(-1), fast = dummy, slow = dummy;//已犯错误：一开始把fast,slow都指向head了，这样没法处理[1]1
+        ListNode dummy = new ListNode(-1), fast = dummy, slow = dummy;// 已犯错误：一开始把fast,slow都指向head了，这样没法处理[1]1
         dummy.next = head;
         for (int i = 0; i < n; i++) fast = fast.next;
         while (fast.next != null) {
@@ -27,5 +27,25 @@ public class RemoveNthNodeFromEndOfList {
         }
         slow.next = slow.next.next;
         return dummy.next;
+    }
+
+    /**
+     * O(2n)写法，先计算长度，再走len - n步。这样没有上面的方法好。
+     */
+    public ListNode removeNthFromEnd_(ListNode head, int n) {
+        ListNode dummy = head;
+        int len = 0;
+        while (head != null) {
+            head = head.next;
+            len++;
+        }
+        if (n == len) return dummy.next;
+        int steps = len - n - 1;
+        head = dummy;
+        while (steps-- > 0) {
+            head = head.next;
+        }
+        if (head != null) head.next = head.next.next;
+        return dummy;
     }
 }
