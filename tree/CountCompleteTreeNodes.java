6052569f99862475ffb74880d0a4f2fa09a9377b
chaangliu <chaangliu@tencent.com>
2020-11-24 21:24:30 +0800

    CountCompleteTreeNodes

---
+++ tree/CountCompleteTreeNodes.java
@@ -0,0 +1,51 @@
+package tree;
+
+/**
+ * 给出一个完全二叉树，求出该树的节点个数。
+ *
+ * 说明：
+ *
+ * 完全二叉树的定义如下：在完全二叉树中，除了最底层节点可能没填满外，其余每层节点数都达到最大值，并且最下面一层的节点都集中在该层最左边的若干位置。若最底层为第 h 层，则该层包含 1~ 2h 个节点。
+ *
+ * 示例:
+ *
+ * 输入:
+ *     1
+ *    / \
+ *   2   3
+ *  / \  /
+ * 4  5 6
+ *
+ * 输出: 6
+ */
+public class CountCompleteTreeNodes {
+    /**
+     * 题意：计算完全二叉树的节点数。
+     * 解法：dfs，利用完全二叉树的性质。
+     * 这个讲解非常清楚：https://leetcode-cn.com/problems/count-complete-tree-nodes/solution/chang-gui-jie-fa-he-ji-bai-100de-javajie-fa-by-xia/
+     * left == right, 说明左子树一定是满二叉树，因为节点已经填充到右子树了，左子树必定已经填满了。所以左子树的节点总数我们可以直接得到，是 2^left - 1，加上当前这个 root 节点，则正好是 2^left。再对右子树进行递归统计。
+     * left != right, 说明此时最后一层不满，但倒数第二层已经满了，可以直接得到右子树的节点个数。同理，右子树节点 +root 节点，总数为 2^right。再对左子树进行递归查找。
+     */
+    public int countNodes(TreeNode root) {
+        if(root == null){
+            return 0;
+        }
+        int left = countLevel(root.left);
+        int right = countLevel(root.right);
+        if(left == right){
+            // left == right, 说明，左子树一定是满二叉树，因为节点已经填充到右子树了，左子树必定已经填满了。所以左子树的节点总数我们可以直接得到，是 2^left - 1，加上当前这个 root 节点，则正好是 2^left。再对右子树进行递归统计。
+            return countNodes(root.right) + (1<<left);
+        }else{
+            // left != right, 说明此时最后一层不满，但倒数第二层已经满了，可以直接得到右子树的节点个数。同理，右子树节点 +root 节点，总数为 2^right。再对左子树进行递归查找。
+            return countNodes(root.left) + (1<<right);
+        }
+    }
+    private int countLevel(TreeNode root){
+        int level = 0;
+        while(root != null){
+            level++;
+            root = root.left;
+        }
+        return level;
+    }
+}
