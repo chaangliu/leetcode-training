eb7e16c601231b148034017e72888d92af6baa30
chaangliu <chaangliu@tencent.com>
2020-12-03 16:06:43 +0800

    count primes review

--- math/CountPrimes.java
+++ math/CountPrimes.java
@@ -1,5 +1,7 @@
 package math;
 
+import java.util.Arrays;
+
 /**
  * Count the number of prime numbers less than a non-negative number, n.
  * <p>
@@ -14,26 +16,28 @@ package math;
 public class CountPrimes {
     /**
      * 题意：计算<n的质数的个数。
-     * 解法：直接计算每个数是否是质数肯定效率很低；
+     * 解法：埃氏筛。直接计算每个数是否是质数肯定效率很低；
      * 这里可以反推，从2开始，一开始假设所有数都是质数，然后从对于每个i * j < n，都把i*j标记成合数。
      * 也就是每个质数，由2开始向后fill合数，所以没有被涉及到的数都是质数。
+     * 优化的方法是j 从i * i开始筛，因为小于i的因子前面都筛过了。
      * 2(false) 4,6,8..(true)
      * 3(false) 9,12..(true)
      * 4(true)
      */
     public int countPrimes(int n) {
-        boolean[] notPrime = new boolean[n];
-        int res = 0;
-        for (int i = 2; i < n; i++) {
-            if (!notPrime[i]) {
-                res++;
-                //for (int j = 2 ; i * j < n && i * j>= 0; j ++){
-                //优化：j = i。但这里要写成 i <= (n-1)/j,否则 i * j 可能超出int范围，但是j = 2 那种写法没事，因为之前已经覆盖到?
-                for (int j = i; i <= (n - 1) / j; j++) {
-                    notPrime[i * j] = true;
+        int[] isPrime = new int[n];
+        Arrays.fill(isPrime, 1);
+        int ans = 0;
+        for (int i = 2; i < n; ++i) {
+            if (isPrime[i] == 1) {
+                ans += 1;
+                if ((long) i * i < n) {
+                    for (int j = i * i; j < n; j += i) { // j += i, not *
+                        isPrime[j] = 0;
+                    }
                 }
             }
         }
-        return res;
+        return ans;
     }
 }
