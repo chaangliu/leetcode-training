6dffbf3b00e1a1539c7641c1875108c7d7d9fdc9
chaangliu <lcconan@gmail.com>
2021-01-03 23:55:26 +0800

    partition list review

--- linkedlist/PartitionList.java
+++ linkedlist/PartitionList.java
@@ -13,29 +13,25 @@ package linkedlist;
  * 20190115
  */
 public class PartitionList {
-
-    ///StraitForward；
-    //这句有点难理解：You should preserve the original relative order of the nodes in each of the two partitions.
-    //意思是只需要保证两个partition的顺序正确就行，不需要pivot放在正确位置；这一点跟quicksort不一样
+    /**
+     * 题意：给你一个链表和一个数字x，把小于x的node放在前面大于等于x的node放在后面。
+     * 解法：直接撸下来放到两个list上，最后在接起来就好了。初学者容易忘记把tail.next置空。
+     */
     public ListNode partition(ListNode head, int x) {
-        ListNode before = new ListNode(-1);
-        ListNode pre1 = before;
-        ListNode after = new ListNode(-1);
-        ListNode pre2 = after;
-
+        ListNode l1 = new ListNode(-1), l2 = new ListNode(-1);
+        ListNode l1Head = l1, l2Head = l2;
         while (head != null) {
             if (head.val < x) {
-                before.next = head;
-                before = before.next;
+                l1.next = head;
+                l1 = l1.next;
             } else {
-                after.next = head;
-                after = after.next;
+                l2.next = head;
+                l2 = l2.next;
             }
             head = head.next;
         }
-        after.next = null;//已犯错误，这一句没有加，导致链表比以前长；LeetCode会提示MLE，我还以为是我用了太多辅助结点，其实是因为没收尾
-        //conjunction
-        before.next = pre2.next;
-        return pre1.next;
+        l1.next = l2Head.next;
+        l2.next = null;
+        return l1Head.next;
     }
 }
--- tree/DiameterOfBinaryTree.java
+++ tree/DiameterOfBinaryTree.java
@@ -5,10 +5,10 @@ package tree;
  * <p>
  * Example:
  * Given a binary tree
- * 1
- * / \
- * 2   3
- * / \
+ *     1
+ *    / \
+ *   2   3
+ *  / \
  * 4   5
  * Return 3, which is the length of the path [4,2,1,3] or [5,2,1,3].
  * <p>
@@ -18,36 +18,10 @@ package tree;
  */
 public class DiameterOfBinaryTree {
     /**
-     * MY CODE
-     * 观察可以发现，此题的结果就是每个node的左子树和右子树深度的和的最大值。但我感觉直接对每个节点重复求解应该会TLE？
-     * --------------
-     * AC了，并没有TLE，复杂度O(n * n)
+     * 题意：求二叉树的直径，也就是二叉树中最长的一条路径。
+     * 利用求二叉树高度的代码就行；有点像binary tree maximum path sum，不需要缓存，在求树的高度的过程中我们已经bottom-up地求过每一个子树的高度了。
      */
     int res = 0;
-
-    public int diameterOfBinaryTree__SLOW(TreeNode root) {
-        if (root == null) return 0;
-        dfs(root);
-        return res;
-    }
-
-    private void dfs(TreeNode root) {
-        if (root == null) return;
-        res = Math.max(res, getDepth(root.left) + getDepth(root.right));
-        dfs(root.left);
-        dfs(root.right);
-    }
-
-    private int getDepth(TreeNode root) {
-        if (root == null) return 0;
-        return Math.max(getDepth(root.left), getDepth(root.right)) + 1;
-    }
-
-    /**
-     * Approach 2
-     * 看了下答案，发现事实上在求树的高度的过程中我们已经bottom-up地求过每一个子树的高度了，所以只要O（n）即可；
-     * dfs return之前保存结果的写法跟前几天做的一道dfs + memo的题很像
-     */
     public int diameterOfBinaryTree(TreeNode root) {
         if (root == null) return 0;
         depth(root);
@@ -56,8 +30,8 @@ public class DiameterOfBinaryTree {
 
     private int depth(TreeNode root) {
         if (root == null) return 0;
-        int ld = getDepth(root.left);
-        int rd = getDepth(root.right);
+        int ld = depth(root.left);
+        int rd = depth(root.right);
         res = Math.max(res, ld + rd);
         return Math.max(ld, rd) + 1;
     }
