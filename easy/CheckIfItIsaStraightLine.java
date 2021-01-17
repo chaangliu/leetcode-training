8eb2f21cf2a3bc2a7b5f7fa9a1a915c4a2808de0
chaangliu <lcconan@gmail.com>
2021-01-17 23:53:56 +0800

    check if straight line

--- easy/CheckIfItIsaStraightLine.java
+++ easy/CheckIfItIsaStraightLine.java
@@ -18,29 +18,31 @@ package easy;
 public class CheckIfItIsaStraightLine {
     /**
      * 题意：判断所有的点是否在一条直线上。
-     * 解法，这题技巧是需要把求斜率转换一下，除法变乘法，这样可以避免divided by zero，以及无需使用double类型。
+     * 解法，这题技巧是需要把求斜率转换一下，对于每个点，以前两个点为基准计算斜率，除法变乘法，这样可以避免divided by zero，以及无需使用double类型。
      */
     public boolean checkStraightLine(int[][] co) {
-        int p = co[0][0], q = co[0][1], u = co[1][0], v = co[1][1];
+        int x0 = co[0][0], y0 = co[0][1], x1 = co[1][0], y1 = co[1][1];
         for (int[] c : co) {
-            if ((c[0] - p) * (c[1] - v) != (c[0] - u) * (c[1] - q))
+            if ((c[0] - x0) * (c[1] - y1) != (c[0] - x1) * (c[1] - y0))
                 return false;
         }
         return true;
     }
 
-    public boolean checkStraightLine_SLOW(int[][] coordinates) {
-        if (coordinates[1][1] - coordinates[0][1] == 0) {
-            for (int[] c : coordinates) {
-                if (c[1] != coordinates[0][1]) return false;
-            }
+    /**
+     * 用除法，要注意*1.0d
+     */
+    public boolean checkStraightLine__(int[][] coordinates) {
+        int n = coordinates.length;
+        if (coordinates[1][0] - coordinates[0][0] == 0) {
+            for (int i = 2; i < n; i++)
+                if (coordinates[i][0] - coordinates[0][0] != 0) return false;
             return true;
         }
-        double stair = (coordinates[1][1] - coordinates[0][1]) * 1.0 / (coordinates[1][0] - coordinates[0][0]);
-        for (int i = 1; i < coordinates.length; i++) {
-            int[] b = coordinates[i], a = coordinates[i - 1];
-            if ((b[0] - a[0]) == 0) return false;
-            if ((b[1] - a[1]) / (b[0] - a[0]) != stair) return false;
+        double k = (coordinates[1][1] - coordinates[0][1]) * 1.0d / (coordinates[1][0] - coordinates[0][0]);
+        for (int i = 2; i < n; i++) {
+            if ((coordinates[i][1] - coordinates[0][1]) * 1.0d / (coordinates[i][0] - coordinates[0][0]) != k)
+                return false;
         }
         return true;
     }
