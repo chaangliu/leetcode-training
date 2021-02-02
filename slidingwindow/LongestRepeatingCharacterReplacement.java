57c0566c3663e50044344a7df9d2f76c6adeaf90
chaangliu <chaangliu@tencent.com>
2021-02-02 16:48:58 +0800

    LongestRepeatingCharacterReplacement review

--- slidingwindow/LongestRepeatingCharacterReplacement.java
+++ slidingwindow/LongestRepeatingCharacterReplacement.java
@@ -34,11 +34,11 @@ import java.util.HashMap;
  */
 public class LongestRepeatingCharacterReplacement {
     /**
-     * 这题我一开始被example误导，理解错了，以为只能替换同一种字母；实际可以替换多个不同字母为1个
-     * <p>
-     * 记录window中出现次数最多的数Target，把其他的数都替换成它；
+     * 题意：给你一个字符串s和k个额度，让你随意替换s中的char最多k次，问替换后最多能组成的全部重复字符的字符串长度是多少。
+     * 这题我一开始被example误导，理解错了，以为只能替换同一个字母；实际可以替换多个不同字母。
+     * 思路是：记录window中出现次数最多的数Target，把其他的数都替换成它；
      * while (right - left + 1 - targetCount > k) -> left ++; else right++;
-     * 维护出现次数最多的数的出现次数maxCharCount，这样就不用在shrink或者expand的时候记录新的target了！因为shrink的时候maxCharCount不会变化
+     * 维护window中出现次数最多的数的出现次数maxCharCount，这样就不用在shrink或者expand的时候记录新的target了！因为shrink的时候maxCharCount不会变化
      */
     public int characterReplacement(String s, int k) {
         int res = 0, left = 0, right = 0;
@@ -52,13 +52,33 @@ public class LongestRepeatingCharacterReplacement {
                 maxCharCount = map.get(rChar);
             }
             while (left < right && right - left + 1 - maxCharCount > k) {
-                map.put(s.charAt(left), map.get(s.charAt(left)) - 1);
+                map.put(s.charAt(left), map.get(s.charAt(left)) - 1); // shrink
                 left++;
             }
             res = Math.max(res, right - left + 1);
             right++;
         }
         return res;
+    }
+
+    /**
+     * 如果不用res，可以直接用right - left，这样最后left到right之间的字符串虽然不是要找的，但是长度一样
+     */
+    public int characterReplacement_Official(String s, int k) {
+        int[] num = new int[26];
+        int n = s.length();
+        int maxn = 0;
+        int left = 0, right = 0;
+        while (right < n) {
+            num[s.charAt(right) - 'A']++;
+            maxn = Math.max(maxn, num[s.charAt(right) - 'A']);
+            if (right - left + 1 - maxn > k) {
+                num[s.charAt(left) - 'A']--;
+                left++;
+            }
+            right++;
+        }
+        return right - left;
     }
 
     /**
@@ -90,58 +110,4 @@ public class LongestRepeatingCharacterReplacement {
         }
         return res;
     }
-
-//    public int characterReplacement(String s, int k) {
-//        int res = 0, left = 0, right = 0;
-//        HashMap<Character, Integer> map = new HashMap<>();
-//        if (s == null || s.length() == 0) return 0;
-//        char target = s.charAt(0);
-//        char viceTarget = target;
-//        while (right < s.length()) {
-//            char rChar = s.charAt(right);
-//            map.put(rChar, map.getOrDefault(rChar, 0) + 1);
-//            if (map.get(rChar) > map.get(target)) {
-//                viceTarget = target;
-//                target = rChar;
-//            }
-//            while (left < right && right - left + 1 - map.get(target) > k) {
-//                map.put(s.charAt(left), map.get(s.charAt(left)) - 1);
-//                if (map.get(target) < map.get(viceTarget)) {
-//                    char tmp = target;
-//                    target = viceTarget;
-//                    viceTarget = tmp;
-//                }
-//                left++;
-//            }
-//            res = Math.max(res, right - left + 1);
-//            right++;
-//        }
-//        return res;
-//    }
-//    public int characterReplacement(String s, int k) {
-//        int res = 0, left = 0, right = 0;
-//        HashMap<Character, Integer> map = new HashMap<>();
-//        int count = 0;//Map中记录的出现次数都大于2的数的个数（关键，有了它就不用遍历map）
-//        int diversity = 0;
-//        while (right < s.length()) {
-//            char lChar = s.charAt(left);
-//            char rChar = s.charAt(right);
-//            map.put(rChar, map.getOrDefault(rChar, 0) + 1);
-//            if (map.get(rChar) == 1)
-//                diversity++;
-//            if (map.get(rChar) == k + 1) count++;
-//            while (left < right && (count > 1 || diversity > 2)) {
-//                map.put(lChar, map.get(lChar) - 1);
-//                if (map.get(lChar) <= k)
-//                    count--;
-//                if (map.get(lChar) == 0)
-//                    diversity--;
-//                left++;
-//            }
-//            res = Math.max(res, right - left + 1);
-//            right++;
-//        }
-//        return res;
-//    }
-
 }
