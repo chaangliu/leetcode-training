97943155eaa41442013ce2ab679579467c6dd576
chaangliu <chaangliu@tencent.com>
2021-03-01 17:05:58 +0800

    num array

--- dp/NumArray.java
+++ dp/NumArray.java
@@ -13,51 +13,23 @@ package dp;
  * 20190807
  */
 class NumArray {
-
     /**
      * 题意：给出一个int数组，求i到j之间的sum。
      * 这题很好但是我猜由于有个corner case(nums是null)导致很多人WA所以很多人点了dislike
      * 一开始我想用二维dp，但是MLE了，然后自然而然想到sum[i, j] = sum[0,j] - sum[0, i-1]这种做法
      */
-    int dp[];
-    int len;
+    int[] dp;
 
     public NumArray(int[] nums) {
-        if (nums == null || nums.length == 0) return;
-        len = nums.length;
-        dp = new int[len];
-        dp[0] = nums[0];
-        for (int i = 1; i < len; i++) {
-            dp[i] = dp[i - 1] + nums[i];
+        dp = new int[nums.length];
+        for (int i = 0; i < dp.length; i++) { // 注意这儿如果从i = 1开始需要初始化dp[0]
+            dp[i] = (i > 0 ? dp[i - 1] : 0) + nums[i];
         }
     }
 
     public int sumRange(int i, int j) {
-        return i == 0 ? dp[j] : dp[j] - dp[i - 1];
+        return dp[j] - (i > 0 ? dp[i - 1] : 0);
     }
-    /**
-     * 二维dp，MLE
-     **/
-    //     int dp [][];
-    //     int len = 0;
-    //     public NumArray(int[] nums) {
-    //         len = nums.length;
-    //         dp = new int[len][len];
-    //         for(int j = 0 ; j < len ; j ++){
-    //             dp[j][j] = nums[j];
-    //             for(int i = 0 ; i < j;  i ++){
-    //                 if(i == 0){
-    //                     dp[i][j] = dp[i][j - 1] + nums[j];
-    //                 }else{
-    //                     dp[i][j] = dp[i - 1][j] - nums[i - 1];   
-    //                 }
-    //             }   
-    //         }
-    //     }
-
-    //     public int sumRange(int i, int j) {
-    //         return dp[i][j];
-    //     }
 }
 
 /**
